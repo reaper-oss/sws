08ceec33bb5beb6a17f876ce94e3510ea975e850
swstim <swstim@users.noreply.github.com>
2010-10-30 15:21:40 +0000

    v1.8.2 #11 for Jeffos's hot fix.

--- version.h
+++ version.h
@@ -26,5 +26,5 @@
 / Auto build # increment code from MS Knowledge base article 237870
 ******************************************************************************/
 
-#define SWS_VERSION			1,8,2,9
-#define SWS_VERSION_STR		"1, 8, 2, 9\0"
+#define SWS_VERSION			1,8,2,11
+#define SWS_VERSION_STR		"1, 8, 2, 11\0"

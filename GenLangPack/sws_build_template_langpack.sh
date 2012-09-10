8bf46a245a49a271e3cd18604c8841b36aa0656b
Jeff0S <Jeff0S@users.noreply.github.com>
2012-09-10 17:59:12 +0000

    Localization: added LangPack generation script in repository Also removed the download "SWS_Localization_Dev_Toolkit.zip" (it was not up-to-date) and edited doc/Wiki accordingly

---
+++ GenLangPack/sws_build_template_langpack.sh
@@ -0,0 +1,4 @@
+mv -f sws-extension sws
+sws_build_sample_langpack.exe --template sws/*.rc sws/*.cpp sws/SnM/*.cpp sws/Autorender/*.cpp sws/Color/*.cpp sws/Console/*.cpp sws/Fingers/*.c?? sws/Freeze/*.cpp sws/IX/*.cpp sws/MarkerActions/*.cpp sws/MarkerList/*.cpp sws/Misc/*.cpp sws/ObjectState/*.cpp sws/Padre/*.cpp sws/Projects/*.cpp sws/Snapshots/*.cpp sws/TrackList/*.cpp sws/Xenakios/*.cpp > sws/SWS_Template.ReaperLangPack
+mv -f sws sws-extension
+

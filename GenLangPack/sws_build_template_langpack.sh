111fe4857e913d498cefcc0ca1df408444825427
Breeder <Breeder@users.noreply.github.com>
2013-01-20 21:59:31 +0000

    Internal (2/2): Updated project files with new BR files Added WDL jnetlib to project files (also added wsock32.lib to win project - used by jnetlib) - used for online version check XCode also removed some of the leftover S&M files Jeffos removed in the last commit sws_build_template_langpack.sh edited to include new folder "Breeder"

--- GenLangPack/sws_build_template_langpack.sh
+++ GenLangPack/sws_build_template_langpack.sh
@@ -1,4 +1,4 @@
 mv -f sws-extension sws
-sws_build_sample_langpack.exe --template sws/*.rc sws/*.cpp sws/SnM/*.cpp sws/Autorender/*.cpp sws/Color/*.cpp sws/Console/*.cpp sws/Fingers/*.c?? sws/Freeze/*.cpp sws/IX/*.cpp sws/MarkerActions/*.cpp sws/MarkerList/*.cpp sws/Misc/*.cpp sws/ObjectState/*.cpp sws/Padre/*.cpp sws/Projects/*.cpp sws/Snapshots/*.cpp sws/TrackList/*.cpp sws/Utility/*.cpp sws/Xenakios/*.cpp > sws/SWS_Template.ReaperLangPack
+sws_build_sample_langpack.exe --template sws/*.rc sws/*.cpp sws/SnM/*.cpp sws/Autorender/*.cpp sws/Breeder/*.cpp sws/Color/*.cpp sws/Console/*.cpp sws/Fingers/*.c?? sws/Freeze/*.cpp sws/IX/*.cpp sws/MarkerActions/*.cpp sws/MarkerList/*.cpp sws/Misc/*.cpp sws/ObjectState/*.cpp sws/Padre/*.cpp sws/Projects/*.cpp sws/Snapshots/*.cpp sws/TrackList/*.cpp sws/Utility/*.cpp sws/Xenakios/*.cpp > sws/SWS_Template.ReaperLangPack
 mv -f sws sws-extension
 

5b0bfbb7e93510e3604150e7870888a2f230ff85
Jeff0S <Jeff0S@users.noreply.github.com>
2013-12-16 19:16:43 +0000

    Caught the hand in the bag (r1155)

--- sws_about.cpp
+++ sws_about.cpp
@@ -43,6 +43,9 @@
 #endif
 
 
+static HWND s_hwndAbout = NULL;
+
+
 void WhatsNew(COMMAND_T*)
 {
 	WDL_FastString fnIn;
@@ -84,58 +87,69 @@ INT_PTR WINAPI doAbout(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	if (INT_PTR r = SNM_HookThemeColorsMessage(hwndDlg, uMsg, wParam, lParam))
 		return r;
 
-	if (uMsg == WM_INITDIALOG)
+	switch(uMsg)
 	{
-		char cVersion[256];
-		sprintf(cVersion, __LOCALIZE_VERFMT("Version %d.%d.%d Build #%d, built on %s","sws_DLG_109"), SWS_VERSION, __DATE__);
-		SetWindowText(GetDlgItem(hwndDlg, IDC_VERSION), cVersion);
+		case WM_INITDIALOG:
+		{
+			char cVersion[256];
+			sprintf(cVersion, __LOCALIZE_VERFMT("Version %d.%d.%d Build #%d, built on %s","sws_DLG_109"), SWS_VERSION, __DATE__);
+			SetWindowText(GetDlgItem(hwndDlg, IDC_VERSION), cVersion);
 #ifdef _WIN64
-		SetDlgItemText(hwndDlg, IDC_LATESTVER, "http://www.standingwaterstudios.com/reaper/sws_extension_x64.exe");
+			SetDlgItemText(hwndDlg, IDC_LATESTVER, "http://www.standingwaterstudios.com/reaper/sws_extension_x64.exe");
 #endif
 #ifndef _WIN32
-		SetDlgItemText(hwndDlg, IDC_LATESTVER, "http://www.standingwaterstudios.com/reaper/sws_osx.dmg");
+			SetDlgItemText(hwndDlg, IDC_LATESTVER, "http://www.standingwaterstudios.com/reaper/sws_osx.dmg");
 #endif
-		bool official, beta; GetStartupSearchOptions(&official, &beta, NULL);
-		CheckDlgButton(hwndDlg, IDC_CHECK1, official);
-		CheckDlgButton(hwndDlg, IDC_CHECK2, beta);
-	}
-	else if (uMsg == WM_DRAWITEM)
-	{
-		DRAWITEMSTRUCT *di = (DRAWITEMSTRUCT *)lParam;
-		if (di->CtlType == ODT_BUTTON) 
-		{
-			if (SWS_THEMING)
-			{
-				if (di->itemState & ODS_SELECTED)
-					SetTextColor(di->hDC, RGB(0,0,0));
-			}
-			else
-				SetTextColor(di->hDC, (di->itemState & ODS_SELECTED) ? RGB(0,0,0) : RGB(0,0,220));
+			bool official, beta; GetStartupSearchOptions(&official, &beta, NULL);
+			CheckDlgButton(hwndDlg, IDC_CHECK1, official);
+			CheckDlgButton(hwndDlg, IDC_CHECK2, beta);
 
-			RECT r = di->rcItem;
-			char buf[512];
-			GetWindowText(di->hwndItem, buf, sizeof(buf));
-			DrawText(di->hDC, buf, -1, &r, DT_NOPREFIX | DT_LEFT | DT_VCENTER);
+			s_hwndAbout = hwndDlg;
+			ShowWindow(hwndDlg, SW_SHOW);
+			break;
 		}
-	}
-	else if (uMsg == WM_COMMAND)
-	{
-		if (wParam == IDC_WEBSITE || wParam == IDC_LATESTVER)
+		case WM_DESTROY:
+			s_hwndAbout = NULL;
+			break;
+		case WM_DRAWITEM:
 		{
-			char cLink[512];
-			GetDlgItemText(hwndDlg, (int)wParam, cLink, 512);
-			ShellExecute(hwndDlg, "open", cLink , NULL, NULL, SW_SHOWNORMAL);
+			DRAWITEMSTRUCT *di = (DRAWITEMSTRUCT *)lParam;
+			if (di->CtlType == ODT_BUTTON) 
+			{
+				if (SWS_THEMING)
+				{
+					if (di->itemState & ODS_SELECTED)
+						SetTextColor(di->hDC, RGB(0,0,0));
+				}
+				else
+					SetTextColor(di->hDC, (di->itemState & ODS_SELECTED) ? RGB(0,0,0) : RGB(0,0,220));
+
+				RECT r = di->rcItem;
+				char buf[512];
+				GetWindowText(di->hwndItem, buf, sizeof(buf));
+				DrawText(di->hDC, buf, -1, &r, DT_NOPREFIX | DT_LEFT | DT_VCENTER);
+			}
+			break;
 		}
-		else if (wParam == IDC_INFO)
-			WhatsNew(NULL);
-		else if (wParam == IDC_LICENSE)
-			DisplayInfoBox(hwndDlg, __LOCALIZE("SWS/S&M Extension - License","sws_DLG_109"), LICENSE_TEXT);
-		else if (wParam == IDC_UPDATE)
-			VersionCheckDialog(hwndDlg);
-		else if (wParam == IDCANCEL)
+		case WM_COMMAND:
 		{
-			SetStartupSearchOptions(!!IsDlgButtonChecked(hwndDlg, IDC_CHECK1), !!IsDlgButtonChecked(hwndDlg, IDC_CHECK2), 0);
-			ShowWindow(hwndDlg, SW_HIDE);
+			if (wParam == IDC_WEBSITE || wParam == IDC_LATESTVER)
+			{
+				char cLink[512];
+				GetDlgItemText(hwndDlg, (int)wParam, cLink, 512);
+				ShellExecute(hwndDlg, "open", cLink , NULL, NULL, SW_SHOWNORMAL);
+			}
+			else if (wParam == IDC_INFO)
+				WhatsNew(NULL);
+			else if (wParam == IDC_LICENSE)
+				DisplayInfoBox(hwndDlg, __LOCALIZE("SWS/S&M Extension - License","sws_DLG_109"), LICENSE_TEXT);
+			else if (wParam == IDC_UPDATE)
+				VersionCheckDialog(hwndDlg);
+			else if (wParam == IDCANCEL) {
+				SetStartupSearchOptions(!!IsDlgButtonChecked(hwndDlg, IDC_CHECK1), !!IsDlgButtonChecked(hwndDlg, IDC_CHECK2), 0);
+				DestroyWindow(hwndDlg);
+			}
+			break;
 		}
 	}
 	return 0;
@@ -143,17 +157,20 @@ INT_PTR WINAPI doAbout(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 void OpenAboutBox(COMMAND_T*)
 {
-	static HWND sHwndAbout = CreateDialog(g_hInst, MAKEINTRESOURCE(IDD_ABOUT), g_hwndParent, doAbout);
-	if (IsWindowVisible(sHwndAbout))
-		ShowWindow(sHwndAbout, SW_HIDE);
-	else
-		ShowWindow(sHwndAbout, SW_SHOW);
+	if (s_hwndAbout)
+		DestroyWindow(s_hwndAbout);
+	else 
+		CreateDialog(g_hInst, MAKEINTRESOURCE(IDD_ABOUT), g_hwndParent, doAbout);
+}
+
+int IsAboutBoxOpen(COMMAND_T*) {
+	return (s_hwndAbout != NULL);
 }
 
 //!WANT_LOCALIZE_1ST_STRING_BEGIN:sws_actions
 static COMMAND_T g_commandTable[] = 
 {
-	{ { DEFACCEL, "SWS: About" }, "SWS_ABOUT", OpenAboutBox, "About SWS Extensions", },
+	{ { DEFACCEL, "SWS: About" }, "SWS_ABOUT", OpenAboutBox, "About SWS Extensions", NULL, IsAboutBoxOpen, },
 	{ { DEFACCEL, "SWS/S&M: What's new?" }, "S&M_WHATSNEW", WhatsNew, },
 	{ { DEFACCEL, "SWS/BR: Check for new SWS version..." }, "BR_VERSION_CHECK", VersionCheckAction, },
 	{ {}, LAST_COMMAND, }, // Denote end of table

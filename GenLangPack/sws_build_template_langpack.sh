2a41145c5650bfe9a51b291715b714dbd3b03a63
Jeff0S <Jeff0S@users.noreply.github.com>
2014-02-10 20:31:07 +0000

    Updated script for automatic LangPack file generation # In order to build LangPack files: # - this script must be performed from <development root> # - the source tree must be in <development root>/sws/
    TODO: wiki update (Building and Localization)

--- GenLangPack/sws_build_template_langpack.sh
+++ GenLangPack/sws_build_template_langpack.sh
@@ -1,4 +1,6 @@
-mv -f sws-extension sws
-sws_build_sample_langpack.exe --template sws/*.rc sws/*.cpp sws/SnM/*.cpp sws/Autorender/*.cpp sws/Breeder/*.cpp sws/Color/*.cpp sws/Console/*.cpp sws/Fingers/*.c?? sws/Freeze/*.cpp sws/IX/*.cpp sws/MarkerActions/*.cpp sws/MarkerList/*.cpp sws/Misc/*.cpp sws/ObjectState/*.cpp sws/Padre/*.cpp sws/Projects/*.cpp sws/Snapshots/*.cpp sws/TrackList/*.cpp sws/Utility/*.cpp sws/Xenakios/*.cpp > sws/SWS_Template.ReaperLangPack
-mv -f sws sws-extension
-
+#!/bin/bash
+# See https://code.google.com/p/sws-extension/wiki/Building and https://code.google.com/p/sws-extension/wiki/Localization
+# In order to build LangPack files:
+# - this script must be performed from <development root>
+# - the source tree must be in <development root>/sws/
+sws/GenLangPack/Release/sws_build_sample_langpack.exe --template sws/*.rc sws/*.cpp sws/SnM/*.cpp sws/Autorender/*.cpp sws/Breeder/*.cpp sws/Color/*.cpp sws/Console/*.cpp sws/Fingers/*.c?? sws/Freeze/*.cpp sws/IX/*.cpp sws/MarkerActions/*.cpp sws/MarkerList/*.cpp sws/Misc/*.cpp sws/ObjectState/*.cpp sws/Padre/*.cpp sws/Projects/*.cpp sws/Snapshots/*.cpp sws/TrackList/*.cpp sws/Utility/*.cpp sws/Xenakios/*.cpp > sws/Install/output/SWS_Template.ReaperLangPack

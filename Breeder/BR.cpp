e85365ef05fbb94fe23bc269d592208671a76878
Breeder <Breeder@users.noreply.github.com>
2014-03-05 12:56:40 +0000

    Action names: more clear naming for new envelope actions

--- Breeder/BR.cpp
+++ Breeder/BR.cpp
@@ -136,10 +136,10 @@ static COMMAND_T g_commandTable[] =
 	{ { DEFACCEL, "SWS/BR: Unselect envelope points outside time selection" },                  "BR_ENV_UNSEL_OUT_TIME_SEL",      SelEnvTimeSel, NULL, -1},
 	{ { DEFACCEL, "SWS/BR: Unselect envelope points in time selection" },                       "BR_ENV_UNSEL_IN_TIME_SEL",       SelEnvTimeSel, NULL, 1},
 
-	{ { DEFACCEL, "SWS/BR: Set envelope point to next point's value" },                         "BR_SET_ENV_TO_NEXT_VAL",         SetEnvValToNextPrev, NULL, 1},
-	{ { DEFACCEL, "SWS/BR: Set envelope point to previous point's value" },                     "BR_SET_ENV_TO_PREV_VAL",         SetEnvValToNextPrev, NULL, -1},
-	{ { DEFACCEL, "SWS/BR: Set envelope point to last selected point's value" },                "BR_SET_ENV_TO_LAST_SEL_VAL",     SetEnvValToNextPrev, NULL, 2},
-	{ { DEFACCEL, "SWS/BR: Set envelope point to first selected point's value" },               "BR_SET_ENV_TO_FIRST_SEL_VAL",    SetEnvValToNextPrev, NULL, -2},
+	{ { DEFACCEL, "SWS/BR: Set selected envelope points to next point's value" },               "BR_SET_ENV_TO_NEXT_VAL",         SetEnvValToNextPrev, NULL, 1},
+	{ { DEFACCEL, "SWS/BR: Set selected envelope points to previous point's value" },           "BR_SET_ENV_TO_PREV_VAL",         SetEnvValToNextPrev, NULL, -1},
+	{ { DEFACCEL, "SWS/BR: Set selected envelope points to last selected point's value" },      "BR_SET_ENV_TO_LAST_SEL_VAL",     SetEnvValToNextPrev, NULL, 2},
+	{ { DEFACCEL, "SWS/BR: Set selected envelope points to first selected point's value" },     "BR_SET_ENV_TO_FIRST_SEL_VAL",    SetEnvValToNextPrev, NULL, -2},
 
 	{ { DEFACCEL, "SWS/BR: Hide all but active envelope for all tracks" },                      "BR_ENV_HIDE_ALL_BUT_ACTIVE",     ShowActiveEnvOnly, NULL, 0},
 	{ { DEFACCEL, "SWS/BR: Hide all but active envelope for selected tracks" },                 "BR_ENV_HIDE_ALL_BUT_ACTIVE_SEL", ShowActiveEnvOnly, NULL, 1},

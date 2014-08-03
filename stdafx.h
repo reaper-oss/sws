81b8ab6aa5fa47738b43bc864f0668860dbf7e55
Breeder <supremekey@gmail.com>
2014-08-04 01:17:40 +0200

    Added WDL_FileRead to stdafx.h

--- stdafx.h
+++ stdafx.h
@@ -86,6 +86,7 @@
 #include "../WDL/win32_utf8.h"
 #include "../WDL/lineparse.h"
 #include "../WDL/MersenneTwister.h"
+#include "../WDL/fileread.h"
 #pragma warning(default : 4996)
 #pragma warning(default : 4267)
 #pragma warning(default : 4244)

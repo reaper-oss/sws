reaper-oss/sws

commit d6c63aeaec1a552911af19dbadd19710770859ee

Analyze and normalize loudness (added true peak support, exporting formatted list, K-offset and misc fixes)
+Analyze loudness dialog:
 - Analyzer can now measure true peak (disabled by default since it prolongs analysis due to resampling - see Options to enable/disable)
  - Double-click true peak cell to move edit cursor to true peak location (or right-click selected item/track in list view to reveal the option)
 - Added user preference to display either LUFS or LU (see Options->Unit).
  - To set reference level for 0 LU and formatting of LU unit, see Options->Global preferences (or run SWS/BR: Global loudness preferences)
  - Right-click menu entry for normalizing follows Options->Unit and will alternate between "Normalize to -23 LUFS" and "Normalize to 0 LU"
  - Normalizing to specific value can normalize to both LUFS and LU
 - Export list of analyzed items/tracks to clipboard or file
  - To export the list, select items in list view, right-click them and select "Export formated list to clipboard/file"
  - For setting the format of exported items, see Options->Export format
   - Formating supports wildcards. Wildcards description can be found in the same dialog. To hide description, simply resize the dialog
   - Besides typing, wildcards can also be inserted using the "Wildcard" button.
   - Previously used format patterns are stored in the "Wildcard" button submenu
 - Other:
  - Right-click menu entry for creating loudness graph no longer queries range, instead it uses range set in Options->Global preferences (or SWS/BR: Global loudness preferences)
  - Normalize dialog is no longer modal (but will be closed if loudness analyzer is closed/hidden to avoid confusion)
  - Display new items/tracks in list view as soon as they're analyzed
  - Analyze button changes caption depending on what is to be analyzed
  - Added Help... menu entry to options menus

Breeder/BR_Loudness.h
@@ -39,28 +39,36 @@ public:
 	~BR_LoudnessObject ();
 
 	/* Analyze */
-	bool Analyze (bool integratedOnly = false);
+	bool Analyze (bool integratedOnly = false, bool doTruePeak = true);
+	void AbortAnalyze ();
 	bool IsRunning ();
 	double GetProgress ();
-	void StopAnalyze ();
-	void NormalizeIntegrated (double targetdB); // normalize using existing analyze data
-	bool CreateGraph (BR_Envelope& envelope, double min, double max, bool momentary);
 
 	/* For populating list view in analyze loudness dialog */
-	double GetColumnVal (int column);
-	void GetColumnStr (int column, char* str, int strSz);
+	double GetColumnVal (int column, int mode);                    // mode: 0->LUFS, 1->LU (LU will follow global format settings)
+	void GetColumnStr (int column, char* str, int strSz, int mode);
 
-	/* Various options (mainly for analyze loudness dialog) */
+	/* Various options and manipulation (mainly for analyze loudness dialog) */
 	double GetAudioLength ();
-	bool CheckTarget (MediaTrack* track);
-	bool CheckTarget (MediaItem_Take* take);
+	double GetAudioStart ();
+	double GetAudioEnd ();
+	double GetMaxMomentaryPos (bool projectTime);
+	double GetMaxShorttermPos (bool projectTime);
+	double GetTruePeakPos (bool projectTime);
 	bool IsTrack ();
 	bool IsTargetValid ();
+	bool CheckTarget (MediaTrack* track);
+	bool CheckTarget (MediaItem_Take* take);
 	bool IsSelectedInProject ();
+	bool CreateGraph (BR_Envelope& envelope, double minLUFS, double maxLUFS, bool momentary);
+	void NormalizeIntegrated (double targetLUFS); // uses existing analyze data
 	void SetSelectedInProject (bool selected);
-	void GotoMomentaryMax (bool timeSelection);
-	void GotoShortTermMax (bool timeSelection);
-	void GotoTarget ();
+	void GoToTarget ();
+	void GoToMomentaryMax (bool timeSelection);
+	void GoToShortTermMax (bool timeSelection);
+	void GoToTruePeak ();
+	int GetTrackNumber ();
+	int GetItemNumber ();
 
 private:
 	struct AudioData
@@ -72,37 +80,115 @@ private:
 		double volume, pan;
 		BR_Envelope volEnv, volEnvPreFX;
 		AudioData();
-	} m_audioData;
+	};
+
 	static unsigned WINAPI AnalyzeData (void* loudnessObject);
-	int CheckSetAudioData (); // call from main thread only, returns 0->target doesn't exist anymore, 1->old accessor still valid, 2->accessor got updated
-	AudioData* GetAudioData ();
+	int CheckSetAudioData (); // call from the main thread only, returns 0->target doesn't exist anymore, 1->old accessor still valid, 2->accessor got updated
+	void SetAudioData (const AudioData& audioData);
+	AudioData GetAudioData ();
 	void SetRunning (bool running);
 	void SetProgress (double progress);
-	void SetAnalyzeData (double integrated, double range, double shortTermMax, double momentaryMax, vector<double>& shortTermValues, vector<double>& momentaryValues);
-	void GetAnalyzeData (double* integrated, double* range, double* shortTermMax, double* momentaryMax);
+	void SetAnalyzeData (double integrated, double range, double truePeak, double truePeakPos, double shortTermMax, double momentaryMax, vector<double>& shortTermValues, vector<double>& momentaryValues);
+	void GetAnalyzeData (double* integrated, double* range, double* truePeak, double* truePeakPos, double* shortTermMax, double* momentaryMax);
 	void SetAnalyzedStatus (bool analyzed);
 	bool GetAnalyzedStatus ();
 	void SetIntegratedOnly (bool integratedOnly);
 	bool GetIntegratedOnly ();
+	void SetDoTruePeak (bool doTruePeak);
+	bool GetDoTruePeak ();
+	void SetTruePeakAnalyzed (bool analyzed);
+	bool GetTruePeakAnalyzeStatus ();
 	void SetKillFlag (bool killFlag);
 	bool GetKillFlag ();
 	void SetProcess (HANDLE process);
 	HANDLE GetProcess ();
 	WDL_FastString GetTakeName ();
 	WDL_FastString GetTrackName ();
-	void operator= (const BR_LoudnessObject&);    // we don't need assignment or copy constructor so disable
+	void operator= (const BR_LoudnessObject&);    // no need for assignment or copy constructor so disable
 	BR_LoudnessObject (const BR_LoudnessObject&); // for now! (it could destroy perfectly valid accessor)
+
+	AudioData m_audioData;
 	MediaTrack* m_track;
 	MediaItem_Take* m_take;
 	GUID m_takeGuid, m_trackGuid;
-	double m_integrated, m_shortTermMax, m_momentaryMax, m_range;
+	double m_integrated, m_truePeak, m_truePeakPos, m_shortTermMax, m_momentaryMax, m_range;
 	double m_progress;
-	bool m_running, m_analyzed, m_killFlag, m_integratedOnly;
+	bool m_running, m_analyzed, m_killFlag, m_integratedOnly, m_doTruePeak, m_truePeakAnalyzed;
 	HANDLE m_process;
 	SWS_Mutex m_mutex;
 	vector<double> m_shortTermValues;
 	vector<double> m_momentaryValues;
 };
+
+/******************************************************************************
+* Loudness preferences and general management/state saving                    *
+******************************************************************************/
+class BR_LoudnessPref
+{
+public:
+	/* No constructor - singleton design */
+	static BR_LoudnessPref& Get ();
+
+	/* These obey active project preferences */
+	double GetGraphMin ();
+	double GetGraphMax ();
+	double GetReferenceLU ();
+	double LUtoLUFS (double lu);
+	double LUFStoLU (double lufs);
+	WDL_FastString GetFormatedLUString ();
+
+	/* Preference dialog */
+	void ShowPreferenceDlg (bool show);
+	void UpdatePreferenceDlg ();
+	bool IsPreferenceDlgVisible ();
+
+	/* Global and project state saving */
+	void SaveGlobalPref ();
+	void LoadGlobalPref ();
+	void SaveProjPref  (ProjectStateContext *ctx);
+	void LoadProjPref  (ProjectStateContext *ctx);
+	void CleanProjPref ();
+
+private:
+	struct ProjData
+	{
+		ProjData ();
+		bool useProjLU, useProjGraph;
+		double valueLU, graphMin, graphMax;
+		WDL_FastString stringLU;
+	};
+
+	BR_LoudnessPref ();
+	BR_LoudnessPref (const BR_LoudnessPref&);
+	void operator=  (const BR_LoudnessPref&);
+	WDL_FastString GetFormatedLUString (int mode, double* valueLU = NULL); // see LUFormat for modes (valueLU is used only in ProjData() constructor)
+	static WDL_DLGRET GlobalLoudnessPrefProc (HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+	SWSProjConfig<BR_LoudnessPref::ProjData> m_projData;
+	HWND m_prefWnd;
+	double m_valueLU, m_graphMin, m_graphMax;
+	int m_globalLUFormat;
+
+	enum LUFormat        {LU = 0, LU_AT_K, LU_K, K, LU_FORMAT_COUNT};
+	enum PrefWndMessages {READ_PROJDATA = 0xF001, SAVE_PROJDATA, UPDATE_LU_FORMAT_PREVIEW};
+};
+
+int LoudnessInit ();
+void LoudnessExit ();
+void LoudnessUpdate (bool updatePreferencesDlg = true);
+
+/******************************************************************************
+* Normalize loudness                                                          *
+******************************************************************************/
+struct BR_NormalizeData
+{
+	WDL_PtrList<BR_LoudnessObject>* items;
+	double targetLufs;
+	bool quickMode;   // analyze integrated loudness only (used when normalizing with actions)
+	bool success;     // did items get successfully normalized?
+};
+
+void NormalizeAndShowProgress (BR_NormalizeData* normalizeData);
 
 /******************************************************************************
 * Analyze loudness list view                                                  *
@@ -117,7 +203,9 @@ protected:
 	virtual void GetItemList (SWS_ListItemList* pList);
 	virtual void OnItemSelChanged (SWS_ListItem* item, int iState);
 	virtual void OnItemDblClk (SWS_ListItem* item, int iCol);
+	virtual void OnItemSortEnd ();
 	virtual int OnItemSort (SWS_ListItem* item1, SWS_ListItem* item2);
+
 };
 
 /******************************************************************************
@@ -129,28 +217,25 @@ public:
 	BR_AnalyzeLoudnessWnd ();
 	~BR_AnalyzeLoudnessWnd () {};
 
-	struct Properties
-	{
-		bool analyzeTracks;
-		bool analyzeOnNormalize;
-		bool mirrorSelection;
-		bool doubleClickGotoTarget;
-		bool timeSelectionOverMax;
-		bool clearEnvelope;
-		bool clearAnalyzed;
-		double graphMin;
-		double graphMax;
-		Properties ();
-		void Load ();
-		void Save ();
-	} m_properties;
+	void Update (bool updateList = true);
+	void KillNormalizeDlg ();
+	bool GetProperty (int propertySpecifier);
+	enum PropertySpecifier {TIME_SEL_OVER_MAX, DOUBLECLICK_GOTO_TARGET, USING_LU, MIRROR_PROJ_SELECTION};
 
 protected:
-	void StopAnalyze ();
-	void StopReanalyze ();
+	BR_LoudnessObject* IsObjectInList (MediaTrack* track);
+	BR_LoudnessObject* IsObjectInList (MediaItem_Take* take);
+	void AbortAnalyze ();
+	void AbortReanalyze ();
 	void ClearList ();
-	bool IsObjectInAnalyzeQueue (MediaTrack* track);
-	bool IsObjectInAnalyzeQueue (MediaItem_Take* take);
+	void ShowExportFormatDialog (bool show);
+	void ShowNormalizeDialog (bool show);
+	void SaveRecentFormatPattern (WDL_FastString pattern);
+	WDL_FastString GetRecentFormatPattern (int id);
+	WDL_FastString GetRecentFormatPatternKey (int id);
+	WDL_FastString CreateExportString (bool previewOnly);
+	static WDL_DLGRET ExportFormatDialogProc (HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+	static WDL_DLGRET NormalizeDialogProc (HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 	virtual void OnInitDlg ();
 	virtual void OnCommand (WPARAM wParam, LPARAM lParam);
 	virtual void OnTimer (WPARAM wParam);
@@ -158,20 +243,45 @@ protected:
 	virtual void GetMinSize (int* w, int* h);
 	virtual int OnKey (MSG* msg, int iKeyState);
 	virtual HMENU OnContextMenu (int x, int y, bool* wantDefaultItems);
+	virtual INT_PTR OnUnhandledMsg(UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+	struct Properties
+	{
+		bool analyzeTracks;
+		bool analyzeOnNormalize;
+		bool mirrorProjSelection;
+		bool doubleClickGoToTarget;
+		bool timeSelOverMax;
+		bool clearEnvelope;
+		bool clearAnalyzed;
+		bool doTruePeak;
+		bool usingLU;
+		WDL_FastString exportFormat;
+		Properties ();
+		void Load ();
+		void Save ();
+	} m_properties;
 	double m_objectsLen;
 	int m_currentObjectId;
 	bool m_analyzeInProgress;
-	BR_AnalyzeLoudnessView* m_list;                                                  // never delete objects in reanalyzeQueue when removing them from list!!
+	BR_AnalyzeLoudnessView* m_list;
+	HWND m_normalizeWnd, m_exportFormatWnd;                                          // never delete objects in reanalyzeQueue when removing them from list!!
 	WDL_PtrList_DeleteOnDestroy<BR_LoudnessObject> m_analyzeQueue, m_reanalyzeQueue; // m_analyzeQueue is ok if the object didn't enter g_analyzedObjects
-};
 
-void AnalyzeLoudness (COMMAND_T*);
-int IsAnalyzeLoudnessVisible (COMMAND_T*);
-int AnalyzeLoudnessInit ();
-void AnalyzeLoudnessExit ();
-void CleanAnalyzeLoudnessProjectData ();
+	enum NormalizeWndMessages    {READ_PROJDATA = 0xF001};
+	enum ExportFormatWndMessages {UPDATE_FORMAT_AND_PREVIEW = 0xF001};
+};
 
 /******************************************************************************
 * Commands                                                                    *
 ******************************************************************************/
 void NormalizeLoudness (COMMAND_T*);
+void AnalyzeLoudness (COMMAND_T*);
+void ToggleLoudnessPref (COMMAND_T* ct);
+
+/******************************************************************************
+* Toggle states                                                               *
+******************************************************************************/
+int IsNormalizeLoudnessVisible (COMMAND_T*);
+int IsAnalyzeLoudnessVisible (COMMAND_T*);
+int IsLoudnessPrefVisible (COMMAND_T*);

Breeder/BR.cpp
@@ -107,11 +107,14 @@ static COMMAND_T g_commandTable[] =
 	/******************************************************************************
 	* Loudness                                                                    *
 	******************************************************************************/
-	{ { DEFACCEL, "SWS/BR: Analyze loudness..." },                               "BR_ANALAYZE_LOUDNESS_DLG",       AnalyzeLoudness, NULL, 0, IsAnalyzeLoudnessVisible},
-	{ { DEFACCEL, "SWS/BR: Normalize loudness of selected tracks..." },          "BR_NORMALIZE_LOUDNESS_TRACKS",   NormalizeLoudness, NULL, 0, },
-	{ { DEFACCEL, "SWS/BR: Normalize loudness of selected tracks to -23 LUFS" }, "BR_NORMALIZE_LOUDNESS_TRACKS23", NormalizeLoudness, NULL, 1, },
-	{ { DEFACCEL, "SWS/BR: Normalize loudness of selected items..." },           "BR_NORMALIZE_LOUDNESS_ITEMS",    NormalizeLoudness, NULL, 2, },
-	{ { DEFACCEL, "SWS/BR: Normalize loudness of selected items to -23 LUFS" },  "BR_NORMALIZE_LOUDNESS_ITEMS23",  NormalizeLoudness, NULL, 3, },
+	{ { DEFACCEL, "SWS/BR: Global loudness preferences..." },                    "BR_LOUDNESS_PREF",                ToggleLoudnessPref, NULL, 0, IsLoudnessPrefVisible},
+	{ { DEFACCEL, "SWS/BR: Analyze loudness..." },                               "BR_ANALAYZE_LOUDNESS_DLG",        AnalyzeLoudness,    NULL, 0, IsAnalyzeLoudnessVisible},
+
+	{ { DEFACCEL, "SWS/BR: Normalize loudness of selected items/tracks..." },    "BR_NORMALIZE_LOUDNESS_ITEMS",     NormalizeLoudness,  NULL, 0, IsNormalizeLoudnessVisible},
+	{ { DEFACCEL, "SWS/BR: Normalize loudness of selected items to -23 LUFS" },  "BR_NORMALIZE_LOUDNESS_ITEMS23",   NormalizeLoudness,  NULL, 1, },
+	{ { DEFACCEL, "SWS/BR: Normalize loudness of selected items to 0 LU" },      "BR_NORMALIZE_LOUDNESS_ITEMS_LU",  NormalizeLoudness,  NULL, -1, },
+	{ { DEFACCEL, "SWS/BR: Normalize loudness of selected tracks to -23 LUFS" }, "BR_NORMALIZE_LOUDNESS_TRACKS23",  NormalizeLoudness,  NULL, 2, },
+	{ { DEFACCEL, "SWS/BR: Normalize loudness of selected tracks to 0 LU" },     "BR_NORMALIZE_LOUDNESS_TRACKS_LU", NormalizeLoudness,  NULL, -2, },
 
 	/******************************************************************************
 	* Midi editor - Media item preview                                            *
@@ -341,14 +344,14 @@ int BR_Init ()
 	SWSRegisterCommands(g_commandTable);
 	InitContinuousActions (); // call only after registering all actions
 	ProjStateInit();
-	AnalyzeLoudnessInit();
+	LoudnessInit();
 	VersionCheckInit();
 	return 1;
 }
 
 void BR_Exit ()
 {
-	AnalyzeLoudnessExit();
+	LoudnessExit();
 }
 
 bool BR_ActionHook (int cmd, int flag)
@@ -359,4 +362,14 @@ bool BR_ActionHook (int cmd, int flag)
 void BR_CSurfSetPlayState (bool play, bool pause, bool rec)
 {
 	MidiTakePreviewPlayState(play, rec);
+}
+
+int BR_CSurfExtended(int call, void* parm1, void* parm2, void* parm3)
+{
+	if (call == CSURF_EXT_RESET)
+	{
+		LoudnessUpdate();
+	}
+
+	return 0;
 }

Breeder/BR.h
@@ -31,3 +31,4 @@ int  BR_Init ();
 void BR_Exit ();
 bool BR_ActionHook (int cmd, int flag);
 void BR_CSurfSetPlayState (bool play, bool pause, bool rec);
+int  BR_CSurfExtended(int call, void* parm1, void* parm2, void* parm3);

Breeder/BR_ProjState.cpp
@@ -125,9 +125,6 @@ static void BeginLoadProjectState (bool isUndo, project_config_extension_t *reg)
 	// Midi note selection
 	g_midiNoteSel.Get()->Empty(true);
 	g_midiNoteSel.Cleanup();
-
-	// Analyze loudness data
-	CleanAnalyzeLoudnessProjectData();
 }
 
 int ProjStateInit ()

resource.h
@@ -79,11 +79,13 @@
 #define IDD_BR_RANDOMIZE_TEMPO          171
 #define IDD_BR_VERSION                  172
 #define IDD_PROMPT_OPTION               173
-#define IDD_BR_ANALYZE_LOUDNESS         174
-#define IDD_BR_NORMALIZE_LOUDNESS       175
-#define IDD_BR_NORMALIZE_LOUDNESS_USER  176
-#define IDD_BR_GRAPH_LOUDNESS           177
+#define IDD_BR_LOUDNESS_ANALYZER        174
+#define IDD_BR_LOUDNESS_ANALYZER_NORM   175
+#define IDD_BR_LOUDNESS_NORM_PROGRESS   176
+#define IDD_BR_LOUDNESS_NORMALIZE       177
 #define IDD_BR_TOOLTIP                  178
+#define IDD_BR_LOUDNESS_PREF            179
+#define IDD_BR_LOUDNESS_EXPORT_FORMAT   180
 #define IDB_UP                          500
 #define IDB_DOWN                        501
 #define IDC_BUTTON1                     1000
@@ -396,14 +398,28 @@
 #define IDC_CANCEL                      1306
 #define IDC_MIN                         1307
 #define IDC_MAX                         1308
+#define IDC_PROJ_LU                     1309
+#define IDC_GLOBAL_LU                   1310
+#define IDC_ENB_PROJ_LU                 1311
+#define IDC_LU_FORMAT                   1312
+#define IDC_TARGET                      1313
+#define IDC_ENB_PROJ_GRAPH              1314
+#define IDC_UNIT                        1315
+#define IDC_VALUE                       1316
+#define IDC_MIN_PROJ                    1317
+#define IDC_MAX_PROJ                    1318
+#define IDC_WILDCARDS                   1319
+#define IDC_FORMAT                      1320
+#define IDC_FORMAT_STATIC               1321
+#define IDC_PREVIEW_STATIC              1322
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        179
+#define _APS_NEXT_RESOURCE_VALUE        181
 #define _APS_NEXT_COMMAND_VALUE         40000
-#define _APS_NEXT_CONTROL_VALUE         1309
+#define _APS_NEXT_CONTROL_VALUE         1323
 #define _APS_NEXT_SYMED_VALUE           100
 #endif
 #endif

sws_extension.rc
@@ -12,7 +12,7 @@
 #undef APSTUDIO_READONLY_SYMBOLS
 
 /////////////////////////////////////////////////////////////////////////////
-// English (U.S.) resources
+// English (United States) resources
 
 #if !defined(AFX_RESOURCE_DLL) || defined(AFX_TARG_ENU)
 #ifdef _WIN32
@@ -1318,21 +1318,33 @@ BEGIN
     CONTROL         "",IDC_BR_VER_PROGRESS,"msctls_progress32",WS_BORDER,7,7,195,15
 END
 
-IDD_BR_ANALYZE_LOUDNESS DIALOGEX 0, 0, 451, 141
-STYLE DS_SETFONT | DS_FIXEDSYS | WS_POPUP | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME
+IDD_BR_LOUDNESS_ANALYZER DIALOGEX 0, 0, 473, 141
+STYLE DS_SETFONT | DS_FIXEDSYS | DS_CENTER | WS_POPUP | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME
 EXSTYLE WS_EX_TOOLWINDOW
 CAPTION "SWS/BR - Analyze loudness"
 FONT 8, "MS Shell Dlg", 400, 0, 0x1
 BEGIN
-    CONTROL         "",IDC_LIST,"SysListView32",LVS_REPORT | LVS_SHOWSELALWAYS | WS_BORDER,6,7,438,113
+    CONTROL         "",IDC_LIST,"SysListView32",LVS_REPORT | LVS_SHOWSELALWAYS | WS_BORDER,6,7,460,113
     EDITTEXT        IDC_EDIT1,48,23,40,12,ES_AUTOHSCROLL | NOT WS_VISIBLE | NOT WS_BORDER
-    CONTROL         "",IDC_PROGRESS,"msctls_progress32",WS_BORDER,126,124,318,11
-    PUSHBUTTON      "Analyze",IDC_ANALYZE,5,123,61,14
-    PUSHBUTTON      "Cancel",IDC_CANCEL,69,123,52,14
-    PUSHBUTTON      "Options",IDC_OPTIONS,392,123,52,14
+    PUSHBUTTON      "Analyze selected items",IDC_ANALYZE,6,123,97,14
+    PUSHBUTTON      "Cancel",IDC_CANCEL,105,123,51,14
+    CONTROL         "",IDC_PROGRESS,"msctls_progress32",WS_BORDER,159,124,252,11
+    PUSHBUTTON      "Options",IDC_OPTIONS,414,123,52,14
 END
 
-IDD_BR_NORMALIZE_LOUDNESS DIALOGEX 0, 0, 264, 29
+IDD_BR_LOUDNESS_ANALYZER_NORM DIALOGEX 0, 0, 187, 49
+STYLE DS_SETFONT | DS_MODALFRAME | DS_FIXEDSYS | DS_CENTER | WS_POPUP | WS_CAPTION | WS_SYSMENU
+CAPTION "SWS/BR - Normalize loudness"
+FONT 8, "MS Shell Dlg", 400, 0, 0x1
+BEGIN
+    LTEXT           "Normalize loudness to:",IDC_STATIC,6,9,73,8
+    EDITTEXT        IDC_VALUE,80,7,40,13,ES_AUTOHSCROLL
+    COMBOBOX        IDC_UNIT,124,7,57,30,CBS_DROPDOWNLIST | WS_VSCROLL | WS_TABSTOP
+    DEFPUSHBUTTON   "Normalize",IDOK,67,28,61,14
+    PUSHBUTTON      "Cancel",IDCANCEL,131,28,50,14
+END
+
+IDD_BR_LOUDNESS_NORM_PROGRESS DIALOGEX 0, 0, 264, 29
 STYLE DS_SETFONT | DS_MODALFRAME | DS_FIXEDSYS | DS_CENTER | WS_POPUP | WS_VISIBLE | WS_CAPTION | WS_SYSMENU
 EXSTYLE WS_EX_TOOLWINDOW
 CAPTION "SWS/BR - Normalizing loudness..."
@@ -1341,37 +1353,68 @@ BEGIN
     CONTROL         "",IDC_PROGRESS,"msctls_progress32",0x0,4,9,256,11
 END
 
-IDD_BR_GRAPH_LOUDNESS DIALOGEX 0, 0, 165, 74
-STYLE DS_SETFONT | DS_MODALFRAME | DS_FIXEDSYS | WS_POPUP | WS_CAPTION | WS_SYSMENU
-CAPTION "SWS/BR - Graph loudness"
-FONT 8, "MS Shell Dlg", 400, 0, 0x1
-BEGIN
-    EDITTEXT        IDC_MIN,69,6,40,14,ES_AUTOHSCROLL
-    LTEXT           "LUFS",IDC_STATIC,114,8,17,8
-    DEFPUSHBUTTON   "Graph",IDOK,6,54,97,14
-    PUSHBUTTON      "Cancel",IDCANCEL,108,54,50,14
-    LTEXT           "Min display value:",IDC_STATIC,6,8,62,8
-    EDITTEXT        IDC_MAX,69,28,40,14,ES_AUTOHSCROLL
-    LTEXT           "LUFS",IDC_STATIC,114,30,17,8
-    LTEXT           "Max display value:",IDC_STATIC,6,30,62,8
-END
-
-IDD_BR_NORMALIZE_LOUDNESS_USER DIALOGEX 0, 0, 188, 52
-STYLE DS_SETFONT | DS_MODALFRAME | DS_FIXEDSYS | WS_POPUP | WS_CAPTION | WS_SYSMENU
+IDD_BR_LOUDNESS_NORMALIZE DIALOGEX 0, 0, 243, 48
+STYLE DS_SETFONT | DS_MODALFRAME | DS_FIXEDSYS | DS_CENTER | WS_POPUP | WS_CAPTION | WS_SYSMENU
 CAPTION "SWS/BR - Normalize loudness"
 FONT 8, "MS Shell Dlg", 400, 0, 0x1
 BEGIN
-    LTEXT           "Normalize loudness to",IDC_STATIC,8,9,70,8
-    EDITTEXT        IDC_EDIT,80,7,40,14,ES_AUTOHSCROLL
-    LTEXT           "LUFS",IDC_STATIC,124,9,17,8
-    DEFPUSHBUTTON   "Normalize",IDOK,68,31,61,14
-    PUSHBUTTON      "Cancel",IDCANCEL,131,31,50,14
+    LTEXT           "Normalize selected",IDC_STATIC,6,9,60,8
+    COMBOBOX        IDC_TARGET,68,7,50,30,CBS_DROPDOWNLIST | WS_VSCROLL | WS_TABSTOP
+    LTEXT           "to:",IDC_STATIC,121,9,10,8
+    EDITTEXT        IDC_VALUE,135,7,40,13,ES_AUTOHSCROLL
+    COMBOBOX        IDC_UNIT,179,7,57,30,CBS_DROPDOWNLIST | WS_VSCROLL | WS_TABSTOP
+    DEFPUSHBUTTON   "Normalize",IDOK,124,27,61,14
+    PUSHBUTTON      "Cancel",IDCANCEL,187,27,50,14
 END
 
 IDD_BR_TOOLTIP DIALOGEX 0, 0, 65, 22
 STYLE DS_SETFONT | DS_FIXEDSYS | DS_CENTER | WS_POPUP | WS_SYSMENU
 FONT 8, "MS Shell Dlg", 400, 0, 0x1
 BEGIN
+END
+
+IDD_BR_LOUDNESS_PREF DIALOGEX 0, 0, 217, 152
+STYLE DS_SETFONT | DS_MODALFRAME | DS_FIXEDSYS | DS_CENTER | WS_POPUP | WS_CAPTION | WS_SYSMENU
+CAPTION "SWS/BR - Global loudness preferences"
+FONT 8, "MS Shell Dlg", 400, 0, 0x1
+BEGIN
+    GROUPBOX        "",IDC_STATIC,7,4,203,66
+    LTEXT           "0 LU reference level:",IDC_STATIC,14,18,68,8
+    COMBOBOX        IDC_GLOBAL_LU,85,16,55,31,CBS_DROPDOWN | WS_VSCROLL | WS_TABSTOP
+    LTEXT           "LUFS",IDC_STATIC,144,18,18,8
+    CONTROL         "Project override:",IDC_ENB_PROJ_LU,"Button",BS_AUTOCHECKBOX | WS_TABSTOP,28,33,67,10
+    COMBOBOX        IDC_PROJ_LU,97,32,43,30,CBS_DROPDOWN | CBS_SORT | WS_VSCROLL | WS_TABSTOP
+    LTEXT           "LUFS",IDC_STATIC,144,35,18,8
+    LTEXT           "LU unit format:",IDC_STATIC,14,52,49,8
+    COMBOBOX        IDC_LU_FORMAT,64,50,76,30,CBS_DROPDOWNLIST | WS_VSCROLL | WS_TABSTOP
+    GROUPBOX        "",IDC_STATIC,7,72,203,51
+    LTEXT           "Envelope graph range:",IDC_STATIC,14,87,74,8
+    EDITTEXT        IDC_MIN,91,84,40,14,ES_AUTOHSCROLL
+    LTEXT           "to",IDC_STATIC,135,87,8,8
+    EDITTEXT        IDC_MAX,146,84,40,14,ES_AUTOHSCROLL
+    LTEXT           "LUFS",IDC_STATIC,189,87,18,8
+    CONTROL         "Project override:",IDC_ENB_PROJ_GRAPH,"Button",BS_AUTOCHECKBOX | WS_TABSTOP,21,103,67,10
+    EDITTEXT        IDC_MIN_PROJ,91,101,40,14,ES_AUTOHSCROLL
+    LTEXT           "to",IDC_STATIC,136,104,8,8
+    EDITTEXT        IDC_MAX_PROJ,146,101,40,14,ES_AUTOHSCROLL
+    LTEXT           "LUFS",IDC_STATIC,189,104,18,8
+    DEFPUSHBUTTON   "OK",IDOK,151,128,59,14
+END
+
+IDD_BR_LOUDNESS_EXPORT_FORMAT DIALOGEX 0, 0, 469, 180
+STYLE DS_SETFONT | DS_FIXEDSYS | DS_CENTER | WS_POPUP | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME
+EXSTYLE WS_EX_TOOLWINDOW
+CAPTION "SWS/BR - Loudness export format"
+FONT 8, "MS Shell Dlg", 400, 0, 0x1
+BEGIN
+    LTEXT           "Format:",IDC_FORMAT_STATIC,6,121,26,8
+    EDITTEXT        IDC_FORMAT,36,119,374,14,ES_AUTOHSCROLL
+    PUSHBUTTON      "Wildcards",IDC_WILDCARDS,413,119,50,14
+    LTEXT           "Preview:",IDC_PREVIEW_STATIC,6,138,29,8
+    EDITTEXT        IDC_PREVIEW,36,136,427,14,ES_AUTOHSCROLL | ES_READONLY
+    DEFPUSHBUTTON   "Save",IDOK,350,157,61,14
+    PUSHBUTTON      "Cancel",IDCANCEL,413,157,50,14
+    EDITTEXT        IDC_EDIT,6,7,457,106,ES_MULTILINE | ES_READONLY | WS_VSCROLL
 END
 
 
@@ -1381,7 +1424,7 @@ END
 //
 
 #ifdef APSTUDIO_INVOKED
-GUIDELINES DESIGNINFO 
+GUIDELINES DESIGNINFO
 BEGIN
     IDD_CONSOLE, DIALOG
     BEGIN
@@ -1882,12 +1925,56 @@ BEGIN
         BOTTOMMARGIN, 48
     END
 
+    IDD_BR_LOUDNESS_ANALYZER, DIALOG
+    BEGIN
+        LEFTMARGIN, 6
+        RIGHTMARGIN, 466
+        TOPMARGIN, 7
+        BOTTOMMARGIN, 137
+    END
+
+    IDD_BR_LOUDNESS_ANALYZER_NORM, DIALOG
+    BEGIN
+        LEFTMARGIN, 6
+        RIGHTMARGIN, 181
+        TOPMARGIN, 7
+        BOTTOMMARGIN, 42
+    END
+
+    IDD_BR_LOUDNESS_NORM_PROGRESS, DIALOG
+    BEGIN
+    END
+
+    IDD_BR_LOUDNESS_NORMALIZE, DIALOG
+    BEGIN
+        LEFTMARGIN, 6
+        RIGHTMARGIN, 237
+        TOPMARGIN, 7
+        BOTTOMMARGIN, 41
+    END
+
     IDD_BR_TOOLTIP, DIALOG
     BEGIN
         LEFTMARGIN, 7
         RIGHTMARGIN, 58
         TOPMARGIN, 7
         BOTTOMMARGIN, 15
+    END
+
+    IDD_BR_LOUDNESS_PREF, DIALOG
+    BEGIN
+        LEFTMARGIN, 7
+        RIGHTMARGIN, 210
+        TOPMARGIN, 4
+        BOTTOMMARGIN, 142
+    END
+
+    IDD_BR_LOUDNESS_EXPORT_FORMAT, DIALOG
+    BEGIN
+        LEFTMARGIN, 6
+        RIGHTMARGIN, 463
+        TOPMARGIN, 7
+        BOTTOMMARGIN, 173
     END
 END
 #endif    // APSTUDIO_INVOKED
@@ -1937,7 +2024,7 @@ IDC_ZOOMIN              CURSOR                  "zoomin.cur"
 IDC_ZOOMOUT             CURSOR                  "zoomout.cur"
 IDC_ZOOMUNDO            CURSOR                  "zoomundo.cur"
 IDC_ZOOMDRAG            CURSOR                  "zoomdrag.cur"
-#endif    // English (U.S.) resources
+#endif    // English (United States) resources
 /////////////////////////////////////////////////////////////////////////////
 
 

commit 72452de3b7dca80ef8d5cdea03a2f28d82909d5a

Skip analysis of already analyzed items when normalizing from loudness dialog (broke it in d6c63aeaec1a552911af19dbadd19710770859ee)

Breeder/BR_Loudness.cpp
@@ -219,7 +219,7 @@ BR_LoudnessObject::~BR_LoudnessObject ()
 		DestroyAudioAccessor(this->GetAudioData().audio);
 }
 
-bool BR_LoudnessObject::Analyze (bool integratedOnly /*=false*/, bool doTruePeak /*=true*/)
+bool BR_LoudnessObject::Analyze (bool integratedOnly, bool doTruePeak)
 {
 	this->AbortAnalyze();
 	this->SetIntegratedOnly(integratedOnly);
@@ -1768,7 +1768,7 @@ static WDL_DLGRET NormalizeProgressProc (HWND hwnd, UINT uMsg, WPARAM wParam, LP
 				if (s_currentItem = s_normalizeData->items->Get(s_currentItemId))
 				{
 					s_currentItemLen = s_currentItem->GetAudioLength();
-					s_currentItem->Analyze(s_normalizeData->quickMode);
+					s_currentItem->Analyze(s_normalizeData->quickMode, false);
 					s_analyzeInProgress = true;
 				}
 				else

Breeder/BR_Loudness.h
@@ -39,7 +39,7 @@ public:
 	~BR_LoudnessObject ();
 
 	/* Analyze */
-	bool Analyze (bool integratedOnly = false, bool doTruePeak = true);
+	bool Analyze (bool integratedOnly, bool doTruePeak);
 	void AbortAnalyze ();
 	bool IsRunning ();
 	double GetProgress ();
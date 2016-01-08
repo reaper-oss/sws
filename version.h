daae260bb3743546e370844e2cfbf1c04e50b8fd
Jeff0S <jeffos277@gmail.com>
2016-01-08 12:37:08 +0100

    v2.8.3

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
-#define SWS_VERSION 2,8,2,0
-#define SWS_VERSION_STR "2, 8, 2, 0\0"
+#define SWS_VERSION 2,8,3,0
+#define SWS_VERSION_STR "2, 8, 3, 0\0"
 #define SWS_VERSION_TYPE "Featured"
 #define REA_VERSION 5,0,0,0

cda05782d345d37ecf71ebd5fa77725dbcb46f6c
Jeff0S <jeffos277@gmail.com>
2016-01-29 14:54:28 +0100

    v2.8.5

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
-#define SWS_VERSION 2,8,4,0
-#define SWS_VERSION_STR "2, 8, 4, 0\0"
+#define SWS_VERSION 2,8,5,0
+#define SWS_VERSION_STR "2, 8, 5, 0\0"
 #define SWS_VERSION_TYPE "Pre-release"
 #define REA_VERSION 5,1,5,0

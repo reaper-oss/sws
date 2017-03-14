a0660cc0dd49a392e3a4c64962c25580048d0986
swstim <tim@standingwaterstudios.com>
2017-03-13 23:36:39 -0500

    v 2.8.8.  Copyright updates.

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
-#define SWS_VERSION 2,8,7,0
-#define SWS_VERSION_STR "2, 8, 7, 0\0"
-#define SWS_VERSION_TYPE "Pre-release"
+#define SWS_VERSION 2,8,8,0
+#define SWS_VERSION_STR "2, 8, 8, 0\0"
+#define SWS_VERSION_TYPE "Featured"
 #define REA_VERSION 5,1,5,0

4636043b9b626aee4712507ebc6405a6197e9696
swstim <tim@standingwaterstudios.com>
2017-03-27 23:15:39 -0500

    v2.9.2

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
-#define SWS_VERSION 2,9,1,0
-#define SWS_VERSION_STR "2, 9, 1, 0\0"
+#define SWS_VERSION 2,9,2,0
+#define SWS_VERSION_STR "2, 9, 2, 0\0"
 #define SWS_VERSION_TYPE "Pre-release"
 #define REA_VERSION 5,2,1,0

f59180ffbc84aa7ad8b89b76f4f5a06d96ef9abc
swstim <tim@standingwaterstudios.com>
2017-04-02 22:16:57 -0500

    Update build.bat for psftp (for internal use)

--- Install/build.bat
+++ Install/build.bat
@@ -7,12 +7,13 @@ REM ===========================================================================
 REM You must set these according to your local setup
 REM FTP access is reserved for project owners
 REM ===========================================================================
-set ftp_host=TO_BE_DEFINED
-set ftp_user=TO_BE_DEFINED
-set ftp_pwd=TO_BE_DEFINED
+set ftp_path="C:\bin\putty\psftp.exe"
+set ftp_host=sws
+set ftp_root=/var/www/standingwaterstudios.com/public_html
 set vcvars_path="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
-set makensis_path="D:\Program Files (x86)\NSIS\makensis.exe"
-set osx_dmg_path="D:\dev\sws_osx.dmg"
+set makensis_path="C:\bin\NSIS\makensis.exe"
+set bash_path="c:\Program Files (x86)\Git\bin\bash.exe"
+set osx_dmg_path="output\sws_osx.dmg"
 
 
 REM ====== INIT ===============================================================
@@ -56,7 +57,7 @@ REM ====== LANGPACK ===========================================================
 :langpack
 echo Generating output\sws_template.ReaperLangPack...
 cd ..\..
-call sws\BuildUtils\build_template_langpack.sh
+%bash_path% sws\BuildUtils\build_template_langpack.sh
 if not errorlevel 0 goto error
 cd sws\Install
 
@@ -88,12 +89,12 @@ REM vcbuild /r /platform:Win32 ..\sws_extension.vcproj release
 REM if errorlevel 1 goto error
 
 REM For MSBuild / Visual Studio 2013
-msbuild /t:rebuild /p:Platform=x64,Configuration=release ..\sws_extension.vcxproj
+msbuild /t:rebuild /m /p:Platform=x64,Configuration=release ..\sws_extension.vcxproj
 if errorlevel 1 goto error
 
 pause
 
-msbuild /t:rebuild /p:Platform=Win32,Configuration=release ..\sws_extension.vcxproj
+msbuild /t:rebuild /m /p:Platform=Win32,Configuration=release ..\sws_extension.vcxproj
 if errorlevel 1 goto error
 
 endlocal
@@ -141,22 +142,20 @@ REM does not release downloaded files while running
 
 REM 1st FTP step: get the current online version
 
-echo user %ftp_user% > temp\get_version.ftp
-echo %ftp_pwd%>> temp\get_version.ftp
-echo cd www/download/%build_type% >> temp\get_version.ftp
-echo ascii >> temp\get_version.ftp
+rem echo user %ftp_user% > temp\get_version.ftp
+rem echo %ftp_pwd%>> temp\get_version.ftp
+echo cd %ftp_root%/download/%build_type% >> temp\get_version.ftp
 echo get version.h temp\online_version.h >> temp\get_version.ftp
 echo quit >> temp\get_version.ftp
 echo FTP: getting current version...
-ftp -v -n -i -s:temp\get_version.ftp %ftp_host%
+%ftp_path% -batch -b temp\get_version.ftp %ftp_host%
 
 
 REM 2nd FTP step: backup remote files and upload new ones
 
-echo user %ftp_user% > temp\upload.ftp
-echo %ftp_pwd%>> temp\upload.ftp
-echo cd www/download/%build_type% >> temp\upload.ftp
-echo binary >> temp\upload.ftp
+rem echo user %ftp_user% > temp\upload.ftp
+rem echo %ftp_pwd%>> temp\upload.ftp
+echo cd %ftp_root%/download/%build_type% >> temp\upload.ftp
 
 REM Backup current remote files into download/old
 if not exist temp\online_version.h (
@@ -171,11 +170,8 @@ if not exist temp\online_version.h (
 ..\BuildUtils\Release\PrintVersion temp\online_version.h "rename tmp.dmg ../old/sws-v%%d.%%d.%%d.%%d.dmg" >> temp\upload.ftp
 ..\BuildUtils\Release\PrintVersion temp\online_version.h "rename sws-v%%d.%%d.%%d.%%d-template.ReaperLangPack tmp.ReaperLangPack" >> temp\upload.ftp
 ..\BuildUtils\Release\PrintVersion temp\online_version.h "rename tmp.ReaperLangPack ../old/sws-v%%d.%%d.%%d.%%d-template.ReaperLangPack" >> temp\upload.ftp
-echo mdelete *.exe >> temp\upload.ftp
-echo mdelete *.dmg >> temp\upload.ftp
-echo mdelete *.ReaperLangPack >> temp\upload.ftp
-echo mdelete *.h >> temp\upload.ftp
-echo mdelete *.html >> temp\upload.ftp
+echo rm *.h >> temp\upload.ftp
+echo rm *.html >> temp\upload.ftp
 REM ... but keep .htm files, e.g. index.htm in the "featured" folder
 
 REM Upload new files
@@ -184,12 +180,11 @@ echo put output\whatsnew.html >> temp\upload.ftp
 ..\BuildUtils\Release\PrintVersion ..\version.h "put output\sws_extension_x64.exe sws-v%%d.%%d.%%d.%%d-x64-install.exe" >> temp\upload.ftp
 ..\BuildUtils\Release\PrintVersion ..\version.h "put %osx_dmg_path% sws-v%%d.%%d.%%d.%%d.dmg" >> temp\upload.ftp
 ..\BuildUtils\Release\PrintVersion ..\version.h "put output\sws_template.ReaperLangPack sws-v%%d.%%d.%%d.%%d-template.ReaperLangPack" >> temp\upload.ftp
-echo ascii >> temp\upload.ftp
 echo put ..\version.h >> temp\upload.ftp
 echo quit >> temp\upload.ftp
 
 echo FTP: uploading...
-ftp -v -n -i -s:temp\upload.ftp %ftp_host%
+%ftp_path% -batch -b temp\upload.ftp %ftp_host%
 echo.
 pause
 goto success

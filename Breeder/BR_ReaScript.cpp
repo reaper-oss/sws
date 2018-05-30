95e063e80f8ec902327807a648d61f5393cba37c
doppgit <som0@meta.ua>
2018-05-30 19:24:55 +0300

    // get vert pos of view in arrange area

--- Breeder/BR_ReaScript.cpp
+++ Breeder/BR_ReaScript.cpp
@@ -1141,3 +1141,15 @@ void NF_SetSWSTrackNotes(MediaTrack* track, const char* buf)
 	NFDoSetSWSTrackNotes(track, buf);
 }
 // /*** nofish stuff ***
+
+
+void DO_GetArrangeVertPos(int* MaxHeightOut, int* ViewPosOut) // dopp func 
+{
+	SCROLLINFO si = { sizeof(SCROLLINFO), };
+	si.fMask = SIF_ALL;
+	CoolSB_GetScrollInfo(GetArrangeWnd(), SB_VERT, &si);
+	int areaHeight = si.nMax + 18;
+	int areaPos = si.nPos;
+	WritePtr(MaxHeightOut, areaHeight);
+	WritePtr(ViewPosOut, areaPos);
+}

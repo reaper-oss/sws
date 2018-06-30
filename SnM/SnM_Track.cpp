reaper-oss/sws

commit c4415d80b1e14c45aaeb0b0b305d4a5d35b25815

Fix "SWS/S&M: Remove all envelopes for selected tracks" (issue 918)

SnM/SnM_Track.cpp
@@ -578,6 +578,10 @@ void ToggleArmTrackEnv(COMMAND_T* _ct)
 		Undo_OnStateChangeEx2(NULL, SWS_CMD_SHORTNAME(_ct), UNDO_STATE_ALL, -1);
 }
 
+// Veto: currently several issues (see #918):
+// 1. Doesn't remove send envs of selected tracks
+// 2. Removes send envs of all tracks which have sends to selected tracks
+// 3. Automation items of removed envs remain in project orphaned (on "Track level" (depth 1), see "<POOLEDENV")
 void RemoveAllEnvsSelTracks(COMMAND_T* _ct)
 {
 	bool updated = false;
@@ -586,11 +590,40 @@ void RemoveAllEnvsSelTracks(COMMAND_T* _ct)
 			if (tr && *(int*)GetSetMediaTrackInfo(tr, "I_SELECTED", NULL)) {
 				SNM_TrackEnvParserPatcher p(tr);
 				updated |= p.RemoveEnvelopes();
-		}
+			}
 	if (updated)
 		Undo_OnStateChangeEx2(NULL, SWS_CMD_SHORTNAME(_ct), UNDO_STATE_ALL, -1);
 }
 
+void RemoveAllEnvsSelTracksNoChunk(COMMAND_T* _ct)
+{
+	bool updated = false;
+	for (int i=0; i <= GetNumTracks(); i++) // incl. master
+		if (MediaTrack* tr = CSurf_TrackFromID(i, false))
+			if (tr && *(int*)GetSetMediaTrackInfo(tr, "I_SELECTED", NULL)) {
+				if (!updated) {
+					PreventUIRefresh(1);
+					Undo_BeginBlock2(NULL);
+					Main_OnCommand(41148, 0); // required for env selection: Envelope: Show all envelopes for (selected) tracks.
+					updated = true;
+				}
+				for (int j=0; j < CountTrackEnvelopes(tr) + j; j++)
+					if (TrackEnvelope* env = GetTrackEnvelope(tr, 0)) {
+						if (CountAutomationItems && GetSetAutomationItemInfo) // AI API optional for now
+							for (int k=0; k < CountAutomationItems(env) + k; k++) {
+								GetSetAutomationItemInfo(env, 0, "D_UISEL", 1, true);
+								Main_OnCommand(42086, 0);  // Envelope: Delete (selected) automation items
+							}
+						SetCursorContext(2, env);   // Select envelope
+						DeleteEnvelopePointRange(env, -1000000000, 1000000000);  //  hack to not spawn a dialog on action 40065
+						Main_OnCommand(40065, 0);   // Envelope: Clear (selected) envelope
+					}
+			}
+	if (updated) {
+		PreventUIRefresh(-1);
+		Undo_EndBlock2(NULL, SWS_CMD_SHORTNAME(_ct), UNDO_STATE_ALL);
+	}
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 // Toolbar track env. write mode toggle

SnM/SnM.cpp
@@ -417,7 +417,7 @@ static COMMAND_T s_cmdTable[] =
 	{ { DEFACCEL, "SWS/S&M: Hide take pitch envelope" }, "S&M_TAKEENV11", ShowHideTakePitchEnvelope, NULL, 0},
 
 	// Track envelopes --------------------------------------------------------
-	{ { DEFACCEL, "SWS/S&M: Remove all envelopes for selected tracks" }, "S&M_REMOVE_ALLENVS", RemoveAllEnvsSelTracks, NULL, },
+	{ { DEFACCEL, "SWS/S&M: Remove all envelopes for selected tracks" }, "S&M_REMOVE_ALLENVS", RemoveAllEnvsSelTracksNoChunk, NULL, },
 	{ { DEFACCEL, "SWS/S&M: Toggle arming of all active envelopes for selected tracks" }, "S&M_TGLARMALLENVS", ToggleArmTrackEnv, NULL, 0, GetFakeToggleState},
 	{ { DEFACCEL, "SWS/S&M: Arm all active envelopes for selected tracks" }, "S&M_ARMALLENVS", ToggleArmTrackEnv, NULL, 1},
 	{ { DEFACCEL, "SWS/S&M: Disarm all active envelopes for selected tracks" }, "S&M_DISARMALLENVS", ToggleArmTrackEnv, NULL, 2},

SnM/SnM_Track.h
@@ -46,6 +46,7 @@ void SelOnlyTrackWithSelEnv(COMMAND_T*);
 bool LookupTrackEnvName(const char* _str, bool _allEnvs);
 void ToggleArmTrackEnv(COMMAND_T*);
 void RemoveAllEnvsSelTracks(COMMAND_T*);
+void RemoveAllEnvsSelTracksNoChunk(COMMAND_T*);
 void ToggleWriteEnvExists(COMMAND_T*);
 int WriteEnvExists(COMMAND_T*);
 

commit d620b07ea870e7ab318afa12d37492f455706a08

fix freeze when running "SWS/S&M: Remove all envelopes for selected tracks" with the master track selected
It broke in commit c4415d80b1e14c45aaeb0b0b305d4a5d35b25815. Fixes #1021.

SnM/SnM_Track.cpp
@@ -598,27 +598,48 @@ void RemoveAllEnvsSelTracks(COMMAND_T* _ct)
 void RemoveAllEnvsSelTracksNoChunk(COMMAND_T* _ct)
 {
 	bool updated = false;
-	for (int i=0; i <= GetNumTracks(); i++) // incl. master
-		if (MediaTrack* tr = CSurf_TrackFromID(i, false))
-			if (tr && *(int*)GetSetMediaTrackInfo(tr, "I_SELECTED", NULL)) {
-				if (!updated) {
-					PreventUIRefresh(1);
-					Undo_BeginBlock2(NULL);
-					Main_OnCommand(41148, 0); // required for env selection: Envelope: Show all envelopes for (selected) tracks.
-					updated = true;
-				}
-				for (int j=0; j < CountTrackEnvelopes(tr) + j; j++)
-					if (TrackEnvelope* env = GetTrackEnvelope(tr, 0)) {
-						if (CountAutomationItems && GetSetAutomationItemInfo) // AI API optional for now
-							for (int k=0; k < CountAutomationItems(env) + k; k++) {
-								GetSetAutomationItemInfo(env, 0, "D_UISEL", 1, true);
-								Main_OnCommand(42086, 0);  // Envelope: Delete (selected) automation items
-							}
-						SetCursorContext(2, env);   // Select envelope
-						DeleteEnvelopePointRange(env, -1000000000, 1000000000);  //  hack to not spawn a dialog on action 40065
-						Main_OnCommand(40065, 0);   // Envelope: Clear (selected) envelope
-					}
+	TrackEnvelope *tempoEnv = NULL;
+
+	const int trackCount = GetNumTracks();
+	for (int ti = 0; ti <= trackCount; ti++) { // incl. master
+		MediaTrack *tr = CSurf_TrackFromID(ti, false);
+
+		if (!tr || *(int*)GetSetMediaTrackInfo(tr, "I_SELECTED", NULL) != 1)
+			continue;
+
+		if (!updated) {
+			PreventUIRefresh(1);
+			Undo_BeginBlock2(NULL);
+			Main_OnCommand(41148, 0); // required for env selection: Envelope: Show all envelopes for (selected) tracks.
+			updated = true;
+		}
+
+		if (ti == 0) // Master track
+			tempoEnv = GetTrackEnvelopeByName(tr, "Tempo map");
+
+		int envIndex = 0;
+		while (CountTrackEnvelopes(tr) > envIndex) {
+			TrackEnvelope* env = GetTrackEnvelope(tr, envIndex);
+
+			if (!env || env == tempoEnv) {
+				++envIndex;
+				continue;
 			}
+
+			if (CountAutomationItems && GetSetAutomationItemInfo) { // AI API optional for now
+				if(const int aiCount = CountAutomationItems(env)) {
+					for(int ai = 0; ai < aiCount; ai++)
+						GetSetAutomationItemInfo(env, ai, "D_UISEL", 1, true);
+					Main_OnCommand(42086, 0);  // Envelope: Delete (selected) automation items
+				}
+			}
+
+			SetCursorContext(2, env);   // Select envelope
+			DeleteEnvelopePointRange(env, -1000000000, 1000000000);  //  hack to not spawn a dialog on action 40065
+			Main_OnCommand(40065, 0);   // Envelope: Clear (selected) envelope
+		}
+	}
+
 	if (updated) {
 		PreventUIRefresh(-1);
 		Undo_EndBlock2(NULL, SWS_CMD_SHORTNAME(_ct), UNDO_STATE_ALL);
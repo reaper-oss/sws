e7a52356f713452c11fa39a65e63630af55493ea
X-Raym <extremraym@yahoo.fr>
2019-01-24 00:05:51 +0100

    2.10.0
    version bump

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
-#define SWS_VERSION 2,9,8,0
-#define SWS_VERSION_STR "2, 9, 8, 0\0"
-#define SWS_VERSION_TYPE "Pre-release"
-#define REA_VERSION 5,5,0,0
+#define SWS_VERSION 2,10,0,0
+#define SWS_VERSION_STR "2, 10, 0, 0\0"
+#define SWS_VERSION_TYPE "Featured"
+#define REA_VERSION 5,9,6,5

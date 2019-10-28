reaper-oss/sws

commit f8a8deeffe29b10ab30256262ee71119014004f5

fix empty columns taking no horizontal space on Linux (#1147)
https://i.imgur.com/uQgmQqf.png

This was happening because SWELL-generic skips list items with no text without updating the coordinates of the next item.

SWS was not setting the LVIF_TEXT mask when populating items beyond the first column thinking it was updating an existing item whose text didn't change.

This patch makes SWS always set LVIF_TEXT when adding items to the list even if the string is empty.

sws_wnd.cpp
@@ -1256,11 +1256,14 @@ void SWS_ListView::Update()
 				{
 					item.iSubItem = iCol;
 					GetItemText(pItem, k, str, sizeof(str));
-					if (!iCol && !bFound)
+					if (!bFound)
 					{
 						item.mask |= LVIF_PARAM | LVIF_TEXT;
 						item.lParam = (LPARAM)pItem;
-						ListView_InsertItem(m_hwndList, &item);
+						if(iCol == 0)
+							ListView_InsertItem(m_hwndList, &item);
+						else
+							ListView_SetItem(m_hwndList, &item);
 						bResort = true;
 					}
 					else

commit ad8866d3d83d2529992328e45a8c64e9ea111710

fix initially blank list view items on Windows
(regression from f8a8deeffe29b10ab30256262ee71119014004f5)

sws_wnd.cpp
@@ -1258,10 +1258,12 @@ void SWS_ListView::Update()
 					GetItemText(pItem, k, str, sizeof(str));
 					if (!bFound)
 					{
-						item.mask |= LVIF_PARAM | LVIF_TEXT;
-						item.lParam = (LPARAM)pItem;
-						if(iCol == 0)
+						item.mask |= LVIF_TEXT;
+						if(iCol == 0) {
+							item.mask |= LVIF_PARAM | LVIF_TEXT;
+							item.lParam = reinterpret_cast<LPARAM>(pItem);
 							ListView_InsertItem(m_hwndList, &item);
+						}
 						else
 							ListView_SetItem(m_hwndList, &item);
 						bResort = true;
reaper-oss/sws

commit 1b02f869cf90c93f2d8d9468f0763a3243b8e63d

extend CF_GetClipboard to allow reading more than 1023 characters
Closes #1171

cfillion/cfillion.cpp
@@ -32,11 +32,11 @@
 #include "version.h"
 
 #ifdef _WIN32
-static const unsigned int FORMAT = CF_UNICODETEXT;
+  static const unsigned int CLIPBOARD_FORMAT = CF_UNICODETEXT;
 #else
-  // on SWELL/generic CF_TEXT may be implemented as a function call which
-  // may not be available until after loading is complete
-#define FORMAT (CF_TEXT)
+// on SWELL/generic CF_TEXT may be implemented as a function call which
+// may not be available until after loading is complete
+#  define CLIPBOARD_FORMAT (CF_TEXT)
 #endif
 
 extern WDL_PtrList_DOD<WDL_FastString> g_script_strs;
@@ -60,57 +60,77 @@ void CF_SetClipboard(const char *buf)
 
   OpenClipboard(GetMainHwnd());
   EmptyClipboard();
-  SetClipboardData(FORMAT, mem);
+  SetClipboardData(CLIPBOARD_FORMAT, mem);
   CloseClipboard();
 }
 
-void CF_GetClipboard(char *buf, int bufSize)
-{
-  OpenClipboard(GetMainHwnd());
-  HANDLE mem = GetClipboardData(FORMAT);
+class ClipboardReader {
+public:
+  ClipboardReader()
+  {
+    OpenClipboard(GetMainHwnd());
+    m_mem = GetClipboardData(CLIPBOARD_FORMAT);
+    m_data = reinterpret_cast<decltype(m_data)>(GlobalLock(m_mem));
 
-  if(void *data = GlobalLock(mem)) {
 #ifdef _WIN32
-    WideCharToMultiByte(CP_UTF8, 0, (const wchar_t *)data, -1,
-      buf, bufSize - 1, nullptr, nullptr);
-
-    // Insert a null terminator if the buffer is too small to hold the entire
-    // clipboard data. WideCharToMultiByte inserts it at the end of the string
-    // by itself when the buffer is big enough.
-    buf[bufSize - 1] = 0;
+    m_size = WideCharToMultiByte(CP_UTF8, 0, m_data, -1, nullptr, 0, nullptr, nullptr) - 1;
 #else
-    snprintf(buf, bufSize, "%s", (const char *)data);
+    m_size = strlen(m_data);
 #endif
-    GlobalUnlock(mem);
   }
 
-  CloseClipboard();
+  // does not null-terminate the output buffer unless its bigger than the
+  // clipboard data
+  void read(char *buf, int bufSize)
+  {
+    if(bufSize > m_size) {
+      bufSize = m_size;
+      buf[m_size] = 0;
+    }
+
+#ifdef _WIN32
+    WideCharToMultiByte(CP_UTF8, 0, m_data, -1, buf, bufSize, nullptr, nullptr);
+#else
+    std::copy(m_data, m_data + bufSize, buf);
+#endif
+  }
+
+  ~ClipboardReader()
+  {
+    GlobalUnlock(m_mem);
+    CloseClipboard();
+  }
+
+  int size() const { return m_size; }
+  operator bool() const { return m_data != nullptr; }
+
+private:
+  HANDLE m_mem;
+#ifdef _WIN32
+  const wchar_t *m_data;
+#else
+  const char *m_data;
+#endif
+  int m_size;
+};
+
+void CF_GetClipboard(char *buf, int bufSize)
+{
+  if(ClipboardReader clipboard{}) {
+    realloc_cmd_ptr(&buf, &bufSize, clipboard.size());
+    clipboard.read(buf, bufSize);
+  }
 }
 
 const char *CF_GetClipboardBig(WDL_FastString *output)
 {
-  if(g_script_strs.Find(output) == -1)
-    return NULL;
+  ClipboardReader clipboard;
 
-  OpenClipboard(GetMainHwnd());
-  HANDLE mem = GetClipboardData(FORMAT);
+  if(!clipboard || g_script_strs.Find(output) == -1)
+    return nullptr;
 
-  if(void *data = GlobalLock(mem)) {
-#ifdef _WIN32
-    const int size = WideCharToMultiByte(CP_UTF8, 0,
-      (const wchar_t *)data, -1, nullptr, 0, nullptr, nullptr) - 1;
-
-    output->SetLen(size);
-
-    WideCharToMultiByte(CP_UTF8, 0, (const wchar_t *)data, -1,
-      const_cast<char *>(output->Get()), size, nullptr, nullptr);
-#else
-    output->Set((const char *)data);
-#endif
-    GlobalUnlock(mem);
-  }
-
-  CloseClipboard();
+  output->SetLen(clipboard.size());
+  clipboard.read(const_cast<char *>(output->Get()), output->GetLength());
 
   return output->Get();
 }

ReaScript.cpp
@@ -299,8 +299,8 @@ APIdef g_apidefs[] =
 	{ APIFUNC(SN_FocusMIDIEditor), "void", "", "", "Focuses the active/open MIDI editor.", },
 
 	{ APIFUNC(CF_SetClipboard), "void", "const char*", "str", "Write the given string into the system clipboard.", },
-	{ APIFUNC(CF_GetClipboard), "void", "char*,int", "buf,buf_sz", "Read the contents of the system clipboard (limited to 1023 characters in Lua).", },
-	{ APIFUNC(CF_GetClipboardBig), "const char*", "WDL_FastString*", "output", "Read the contents of the system clipboard. See <a href=\"#SNM_CreateFastString\">SNM_CreateFastString</a> and <a href=\"#SNM_DeleteFastString\">SNM_DeleteFastString</a>.", },
+	{ APIFUNC(CF_GetClipboard), "void", "char*,int", "bufNeedBig,buf_sz", "Read the contents of the system clipboard.", },
+	{ APIFUNC(CF_GetClipboardBig), "const char*", "WDL_FastString*", "output", "[DEPRECATED: Use <a href=\"#CF_GetClipboard\">CF_GetClipboard</a>] Read the contents of the system clipboard. See <a href=\"#SNM_CreateFastString\">SNM_CreateFastString</a> and <a href=\"#SNM_DeleteFastString\">SNM_DeleteFastString</a>.", },
 	{ APIFUNC(CF_ShellExecute), "bool", "const char*", "file", "Open the given file or URL in the default application. See also <a href=\"#CF_LocateInExplorer\">CF_LocateInExplorer</a>.", },
 	{ APIFUNC(CF_LocateInExplorer), "bool", "const char*", "file", "Select the given file in explorer/finder.", },
 

sws_extension.cpp
@@ -982,6 +982,7 @@ error:
 		IMPAPI(PreventUIRefresh);
 		IMPAPI(projectconfig_var_addr);
 		IMPAPI(projectconfig_var_getoffs);
+		IMPAPI(realloc_cmd_ptr); // v5.965+
 		IMPAPI(RefreshToolbar);
 		IMPAPI(RefreshToolbar2); // v5pre8+
 #ifdef _WIN32

commit 24ec6f3e5d24e684f20f725f888eab74b3fe0e74

fix crash in CF_GetClipboard{,Big} when there is no text data
Regression from commit 1b02f869cf90c93f2d8d9468f0763a3243b8e63d.

cfillion/cfillion.cpp
@@ -72,6 +72,11 @@ public:
     m_mem = GetClipboardData(CLIPBOARD_FORMAT);
     m_data = reinterpret_cast<decltype(m_data)>(GlobalLock(m_mem));
 
+    if(!m_data) {
+      m_size = 0;
+      return;
+    }
+
 #ifdef _WIN32
     m_size = WideCharToMultiByte(CP_UTF8, 0, m_data, -1, nullptr, 0, nullptr, nullptr) - 1;
 #else
@@ -87,6 +92,9 @@ public:
       bufSize = m_size;
       buf[m_size] = 0;
     }
+
+    if(!bufSize)
+      return;
 
 #ifdef _WIN32
     WideCharToMultiByte(CP_UTF8, 0, m_data, -1, buf, bufSize, nullptr, nullptr);
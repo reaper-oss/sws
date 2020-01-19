reaper-oss/sws

commit 81fc7a4a553ee2f99797570b0f49fd5a3c381ea9

rewrite SaveProjectList and OpenProjectsFromList in C++
Now writes \n on Linux and macOS and DOS-style \r\n only on Windows

Projects/ProjectMgr.cpp
@@ -42,13 +42,17 @@ static SWSProjConfig<WDL_PtrList_DOD<WDL_String> > g_relatedProjects;
 // ****************************************************************************
 void SaveProjectList(COMMAND_T*)
 {
+	std::vector<std::string> projectFiles;
+
 	int i = 0;
-	bool bValid = false;
-	char filename[MAX_PATH + 2]{}; // room for \r\n
-	while (EnumProjects(i++, filename, MAX_PATH))
+	char filename[MAX_PATH]{};
+	while (EnumProjects(i++, filename, sizeof(filename)))
+	{
 		if (filename[0])
-			bValid = true;
-	if (!bValid)
+			projectFiles.push_back(filename);
+	}
+
+	if (projectFiles.empty())
 	{
 		MessageBox(g_hwndParent, __LOCALIZE("No saved projects are open. Please save your project(s) first.","sws_mbox"), __LOCALIZE("SWS Project List Save","sws_mbox"), MB_OK);
 		return;
@@ -56,74 +60,72 @@ void SaveProjectList(COMMAND_T*)
 
 	char cPath[MAX_PATH];
 	GetProjectPath(cPath, sizeof(cPath));
-	if (BrowseForSaveFile(__LOCALIZE("Select project list","sws_mbox"), cPath, NULL, "Reaper Project List (*.RPL)\0*.RPL\0All Files\0*.*\0", filename, MAX_PATH))
+	if (!BrowseForSaveFile(__LOCALIZE("Select project list","sws_mbox"), cPath, nullptr, "Reaper Project List (*.RPL)\0*.RPL\0All Files\0*.*\0", filename, sizeof(filename)))
+		return;
+
+	std::ofstream file(win32::widen(filename).c_str());
+
+	if (!file)
 	{
-		FILE* f = fopenUTF8(filename, "w");
-		if (f)
-		{
-			i = 0;
-			while (EnumProjects(i++, filename, MAX_PATH))
-			{
-				if (filename[0])
-				{
-					strcat(filename, "\r\n");
-					fwrite(filename, strlen(filename), 1, f);
-				}
-			}
-			fclose(f);
-		}
-		else
-			MessageBox(g_hwndParent, __LOCALIZE("Unable to write to file.","sws_mbox"), __LOCALIZE("SWS Project List Save","sws_mbox"), MB_OK);
+		MessageBox(g_hwndParent, __LOCALIZE("Unable to write to file.","sws_mbox"), __LOCALIZE("SWS Project List Save","sws_mbox"), MB_OK);
+		return;
 	}
+
+	for (const std::string &projectfn : projectFiles)
+		file << projectfn << "\n";
+
+	file.close();
 }
 
 void OpenProjectsFromList(COMMAND_T*)
 {
-	char cPath[256];
-	GetProjectPath(cPath, 256);
-	char* filename = BrowseForFiles(__LOCALIZE("Select project list","sws_mbox"), cPath, NULL, false, "Reaper Project List (*.RPL)\0*.RPL\0All Files\0*.*\0");
-	if (filename)
+	char directory[MAX_PATH]{};
+	GetProjectPath(directory, sizeof(directory));
+
+	char *filename = BrowseForFiles(__LOCALIZE("Select project list","sws_mbox"), directory, nullptr, false, "Reaper Project List (*.RPL)\0*.RPL\0All Files\0*.*\0");
+	if (!filename)
+		return;
+
+	std::ifstream file(win32::widen(filename).c_str());
+	free(filename);
+
+	if (!file)
 	{
-		FILE* f = fopenUTF8(filename, "r");
-		if (f)
-		{
-			// Save "prompt on new project" variable
-			ConfigVarOverride<int> newprojdo("newprojdo", 0);
-
-			int i = 0;
-
-			int iProjects = -1;
-			while (EnumProjects(++iProjects, NULL, 0)); // Count projects
-			char cName[10];
-			EnumProjects(-1, cName, 10);
-
-			if (iProjects != 1 || cName[0] != 0 || GetNumTracks() != 0)
-			{
-				if (MessageBox(g_hwndParent, __LOCALIZE("Close active tabs first?","sws_mbox"), __LOCALIZE("SWS Project List Open","sws_mbox"), MB_YESNO) == IDYES)
-					Main_OnCommand(40886, 0);
-				else
-					i = 1;
-			}
-
-			while(fgets(cPath, 256, f))
-			{
-				char* pC;
-				while((pC = strchr(cPath, '\r'))) *pC = 0; // Strip newlines no matter the format
-				while((pC = strchr(cPath, '\n'))) *pC = 0;
-				if (cPath[0])
-				{
-					if (i++)
-						Main_OnCommand(41929, 0); // New project tab (ignore default template)
-					Main_openProject(cPath);
-				}
-			}
-			fclose(f);
-		}
-		else
-			MessageBox(g_hwndParent, __LOCALIZE("Unable to open file.","sws_mbox"), __LOCALIZE("SWS Project List Open","sws_mbox"), MB_OK);
-		
-		free(filename);
+		MessageBox(g_hwndParent, __LOCALIZE("Unable to open file.","sws_mbox"), __LOCALIZE("SWS Project List Open","sws_mbox"), MB_OK);
+		return;
 	}
+
+	// Save "prompt on new project" variable
+	ConfigVarOverride<int> newprojdo("newprojdo", 0);
+
+	int projectCount = 0;
+	while (EnumProjects(projectCount++, nullptr, 0)); // Count projects
+
+	char projectfn[10];
+	EnumProjects(-1, projectfn, sizeof(projectfn));
+
+	if (projectCount > 1 || projectfn[0] || GetNumTracks() > 0)
+	{
+		if (MessageBox(g_hwndParent, __LOCALIZE("Close active tabs first?","sws_mbox"), __LOCALIZE("SWS Project List Open","sws_mbox"), MB_YESNO) == IDYES)
+		{
+			Main_OnCommand(40886, 0); // File: Close all projects
+			projectCount = 0;
+		}
+	}
+
+	std::string line;
+	while (std::getline(file, line))
+	{
+		if(line.empty())
+			continue;
+
+		if (projectCount > 0)
+			Main_OnCommand(41929, 0); // New project tab (ignore default template)
+
+		Main_openProject(line.c_str());
+	}
+
+	file.close();
 }
 
 // ****************************************************************************

commit 1934c0e84579dbb09f4492e26436508b9b7b9c01

fix opening projects from list not creating new tabs if active tabs were closed
Regression from commit 81fc7a4a553ee2f99797570b0f49fd5a3c381ea9.

Fixes #1293

Projects/ProjectMgr.cpp
@@ -98,29 +98,32 @@ void OpenProjectsFromList(COMMAND_T*)
 	// Save "prompt on new project" variable
 	ConfigVarOverride<int> newprojdo("newprojdo", 0);
 
-	int projectCount = 0;
-	while (EnumProjects(projectCount++, nullptr, 0)); // Count projects
+	int projectCount = -1;
+	while (EnumProjects(++projectCount, nullptr, 0));
 
 	char projectfn[10];
 	EnumProjects(-1, projectfn, sizeof(projectfn));
 
+	bool newTab = false;
+
 	if (projectCount > 1 || projectfn[0] || GetNumTracks() > 0)
 	{
 		if (MessageBox(g_hwndParent, __LOCALIZE("Close active tabs first?","sws_mbox"), __LOCALIZE("SWS Project List Open","sws_mbox"), MB_YESNO) == IDYES)
-		{
 			Main_OnCommand(40886, 0); // File: Close all projects
-			projectCount = 0;
-		}
+		else
+			newTab = true;
 	}
 
 	std::string line;
 	while (std::getline(file, line))
 	{
-		if(line.empty())
+		if (line.empty())
 			continue;
 
-		if (projectCount > 0)
+		if (newTab)
 			Main_OnCommand(41929, 0); // New project tab (ignore default template)
+		else
+			newTab = true;
 
 		Main_openProject(line.c_str());
 	}
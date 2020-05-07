reaper-oss/sws

commit c06f8cfa0d64607c4544a733ae780af21a9d2439

harden configvar against invalid type interpretation
Fixes #1131

Misc/EditCursor.cpp
@@ -94,17 +94,18 @@ void MoveCursorAndSel(COMMAND_T* ct)
 void MoveCursorSample(COMMAND_T* ct)
 {
 	double dPos = GetCursorPosition();
-	int* pSrate = (int*)GetConfigVar("projsrate");
-	if (!pSrate)
+
+	const ConfigVar<int> projsrate("projsrate");
+	if (!projsrate)
 		return;
-	double dSrate = (double)*pSrate;
-	INT64 iCurSample = (INT64)(dPos * dSrate + 0.5);
-	if (ct->user == -1 && (dPos == (double)(iCurSample / dSrate)))
+
+	INT64 iCurSample = (INT64)(dPos * *projsrate + 0.5);
+	if (ct->user == -1 && (dPos == (double)(iCurSample / *projsrate)))
 		iCurSample--;
 	else if (ct->user == 1)
 		iCurSample++;
 
-	double dNewPos = (double)(iCurSample / dSrate);
+	double dNewPos = (double)(iCurSample / *projsrate);
 
 	SetEditCurPos(dNewPos, true, false);
 }
@@ -121,7 +122,7 @@ void MoveCursorMs(COMMAND_T* ct)
 void MoveCursorFade(COMMAND_T* ct)
 {
 	double dPos = GetCursorPosition();
-	dPos += fabs(*(double*)GetConfigVar("deffadelen")) * (double)ct->user; // Abs because neg value means "not auto"
+	dPos += fabs(*ConfigVar<double>("deffadelen")) * (double)ct->user; // Abs because neg value means "not auto"
 	SetEditCurPos(dPos, true, false);
 }
 

Breeder/BR.cpp
@@ -96,11 +96,12 @@ void BR_MenuHook (COMMAND_T* ct, HMENU menu, int id)
 	{
 		// Disable the action "Flush FX on stop" if the option "Run FX when stopped" is turned off
 		if ((int)ct->user == 1)
-			EnableMenuItem(menu, id, (GetBit(*static_cast<int*>(GetConfigVar("runallonstop")), 0) ? MF_ENABLED : MF_GRAYED) | MF_BYPOSITION);
+			EnableMenuItem(menu, id,
+				(GetBit(*ConfigVar<int>("runallonstop"), 0) ? MF_ENABLED : MF_GRAYED) | MF_BYPOSITION);
 
 		// Disable the action to set "Run FX after stopping for" if the option "Run FX when stopped" is turned on
 		else if ((int)ct->user <= 0)
-			EnableMenuItem(menu, id, (!GetBit(*static_cast<int*>(GetConfigVar("runallonstop")), 0) ? MF_ENABLED : MF_GRAYED) | MF_BYPOSITION);
+			EnableMenuItem(menu, id, (!GetBit(*ConfigVar<int>("runallonstop"), 0) ? MF_ENABLED : MF_GRAYED) | MF_BYPOSITION);
 	}
 }
 

Breeder/BR_ContextualToolbars.cpp
@@ -246,7 +246,7 @@ void BR_ContextualToolbar::LoadToolbar (bool exclusive)
 				bool undoTake  = this->ActivateTake(executeOnToolbarLoad);
 
 				// Create undo point
-				int undoMask; GetConfig("undomask", undoMask);
+				const int undoMask = ConfigVar<int>("undomask").value_or(0);
 				if (!GetBit(undoMask, 0)) // "Create undo points for item/track selection" is off
 				{
 					undoTrack = false;

Breeder/BR_ContinuousActions.cpp
@@ -226,7 +226,8 @@ static void KillNativeTooltip ()
 {
 	if (g_actionInProgress && !g_removedNativeTooltip)
 	{
-		SetConfig("tooltips", SetBit(SetBit(SetBit(g_tooltips, 0), 1), 2));
+		ConfigVar<int>("tooltips").try_set(SetBit(SetBit(SetBit(g_tooltips, 0), 1), 2));
+
 		for (list<HWND>::const_iterator it = g_registeredWindows.begin(), end = g_registeredWindows.end(); it != end; ++it)
 			InvalidateRect(*it,  NULL, TRUE);
 		g_removedNativeTooltip = true;
@@ -379,7 +380,7 @@ static bool ContinuousActionInit (bool init, COMMAND_T* ct, HWND hwnd, BR_Contin
 	if (initSuccessful)
 	{
 		// Reset variables
-		GetConfig("tooltips", g_tooltips);
+		g_tooltips = ConfigVar<int>("tooltips").value_or(0);
 		g_actionInProgress     = action;
 		g_removedNativeTooltip = false;
 		g_midiEditorWnd        = (action && action->ct->uniqueSectionId == SECTION_MIDI_EDITOR) ? hwnd                          : NULL;
@@ -476,7 +477,7 @@ static bool ContinuousActionInit (bool init, COMMAND_T* ct, HWND hwnd, BR_Contin
 		// Reset tooltips options in case they were changed and remove our tooltip
 		if (g_removedNativeTooltip)
 		{
-			SetConfig("tooltips", g_tooltips);
+			ConfigVar<int>("tooltips").try_set(g_tooltips);
 
 			// Make sure native tooltip is draw before hiding our tooltip
 			POINT p; GetCursorPos(&p);

Breeder/BR_Envelope.cpp
@@ -312,12 +312,15 @@ static void SetEnvPointMouseValue (COMMAND_T* ct)
 		swap(endDisplayVal, startDisplayVal);
 		oppositeDirection = true;
 	}
-	double endPosQN = (isTempo) ? TimeMap_timeToQN_abs(NULL, endPosition) : 0;
+	double endPosQN = isTempo ? TimeMap_timeToQN_abs(NULL, endPosition) : 0;
 
-	int envClickSegMode; GetConfig("envclicksegmode", envClickSegMode);
-	int editCursorUndo;  GetConfig("undomask",        editCursorUndo);
-	SetConfig("undomask",        ClearBit(editCursorUndo, 3));  // prevent edit cursor undo
-	SetConfig("envclicksegmode", ClearBit(envClickSegMode, 6)); // prevent reselection
+	// prevent edit cursor undo
+	const ConfigVar<int> editCursorUndo("undomask");
+	ConfigVarOverride<int> tempMask(editCursorUndo, ClearBit(editCursorUndo.value_or(0), 3));
+
+	// prevent reselection
+	const ConfigVar<int> envClickSegMode("envclicksegmode");
+	ConfigVarOverride<int> tempSegMode(envClickSegMode, ClearBit(envClickSegMode.value_or(0), 6));
 
 	// In case of tempo freehand drawing we simultaneously add/remove points and edit them (because editing tempo map makes things move and me must not allow any changes to anything in front of mouse cursor)
 	if (user == 3 && isTempo)
@@ -608,9 +611,6 @@ static void SetEnvPointMouseValue (COMMAND_T* ct)
 		g_envMouseDidOnce = true;
 	}
 
-	SetConfig("undomask",        editCursorUndo);
-	SetConfig("envclicksegmode", envClickSegMode);
-
 	// Swap values back before storing them
 	if (oppositeDirection)
 	{
@@ -744,8 +744,10 @@ void CursorToEnv1 (COMMAND_T* ct)
 			GetSet_LoopTimeRange2(NULL, false, false, &tStart, &tEnd, false);
 
 			// Enable selection of points in time selection
-			int envClickSegMode; GetConfig("envclicksegmode", envClickSegMode);
-			SetConfig("envclicksegmode", SetBit(envClickSegMode, 6));
+			ConfigVar<int> envClickSegMode("envclicksegmode");
+			const int oldEnvClickSegMode = envClickSegMode.value_or(0);
+			ConfigVarOverride<int> tmpEnvClickSegMode("envclicksegmode",
+				SetBit(oldEnvClickSegMode, 6));
 
 			// Set time selection that in turn sets new envelope selection
 			nStart = (cTime + pTime) / 2 + takeEnvStartPos;
@@ -753,11 +755,8 @@ void CursorToEnv1 (COMMAND_T* ct)
 			GetSet_LoopTimeRange2(NULL, true, false, &nStart, &nEnd, false);
 
 			// Preserve point when previous time selection gets restored
-			SetConfig("envclicksegmode", ClearBit(envClickSegMode, 6));
+			envClickSegMode.try_set(ClearBit(oldEnvClickSegMode, 6));
 			GetSet_LoopTimeRange2(NULL, true, false, &tStart, &tEnd, false);
-
-			// Restore preferences
-			SetConfig("envclicksegmode", envClickSegMode);
 
 			PreventUIRefresh(-1);
 		}
@@ -986,7 +985,7 @@ void EnvPointsGrid (COMMAND_T* ct)
 	// Readjust tempo points
 	if (deletePoints && pointsToDelete.size() > 0)
 	{
-		int timeBase; GetConfig("tempoenvtimelock", timeBase);
+		const int timeBase = ConfigVar<int>("tempoenvtimelock").value_or(0);
 		if ((envelope.IsTempo() && timeBase != 0))
 		{
 			double offset = 0;
@@ -2056,7 +2055,7 @@ void ShowHideSendEnv (COMMAND_T* ct)
 				if (!envelope->IsVisible() && envelope->CountPoints() <= 1)
 				{
 					double value;
-					int trimMode; GetConfig("envtrimadjmode", trimMode);
+					const int trimMode = ConfigVar<int>("envtrimadjmode").value_or(0);
 					if (envelope->GetPoint(0, NULL, &value, NULL, NULL) && (trimMode == 0 || (trimMode == 1 && GetEffectiveAutomationMode(envelope->GetParent()) != 0)))
 					{
 						if      (envelope->Type() == VOLUME) SetTrackSendUIVol(envelope->GetParent(), envelope->GetSendId(), value, 0); // don't do mute (REAPER skips it too)

Breeder/BR_EnvelopeUtil.cpp
@@ -649,7 +649,7 @@ double BR_Envelope::ValueAtPosition (double position, bool fastMode /*= false*/)
 	if (!m_pointsEdited && !fastMode)
 	{
 		if (m_sampleRate == -1)
-			GetConfig("projsrate", m_sampleRate);
+			m_sampleRate = ConfigVar<int>("projsrate").value_or(-1);
 
 		double value;
 		Envelope_Evaluate(m_envelope, position, m_sampleRate, 1, &value, NULL, NULL, NULL); // slower than our way with high point count (probably because we use binary search while Cockos uses linear) but more accurate
@@ -817,7 +817,7 @@ double BR_Envelope::SnapValue (double value)
 {
 	if (this->Type() == PITCH)
 	{
-		int pitchenvrange; GetConfig("pitchenvrange", pitchenvrange);
+		const int pitchenvrange = ConfigVar<int>("pitchenvrange").value_or(0);
 		int mode = pitchenvrange >> 8; // range is in high 8 bits
 
 		if      (mode == 0) return value;
@@ -1299,12 +1299,12 @@ double BR_Envelope::LaneMinValue ()
 {
 	if (m_tempoMap)
 	{
-		int min; GetConfig("tempoenvmin", min);
+		const int min = ConfigVar<int>("tempoenvmin").value_or(0);
 		return (double)min;
 	}
 	else if (this->Type() == PITCH)
 	{
-		int min; GetConfig("pitchenvrange", min);
+		const int min = ConfigVar<int>("pitchenvrange").value_or(0);
 		return -(min & 0x0F); // range is in low 8 bits
 	}
 	return this->MinValueAbs();
@@ -1314,14 +1314,14 @@ double BR_Envelope::LaneMaxValue ()
 {
 	if (m_tempoMap)
 	{
-		int max; GetConfig("tempoenvmax", max);
+		const int max = ConfigVar<int>("tempoenvmax").value_or(0);
 		return (double)max;
 	}
 	else if (this->Type() == VOLUME || this->Type() == VOLUME_PREFX)
 	{
 		static int s_max = -666;
 		static int s_val = -666;
-		int max; GetConfig("volenvrange", max);
+		const int max = ConfigVar<int>("volenvrange").value_or(0);
 
 		// LaneMaxValue tends to get called a lot so instead of doing all comparisons each time, rather cache value and do one comparison only
 		if (max != s_max)
@@ -1336,7 +1336,7 @@ double BR_Envelope::LaneMaxValue ()
 	}
 	else if (this->Type() == PITCH)
 	{
-		int max; GetConfig("pitchenvrange", max);
+		const int max = ConfigVar<int>("pitchenvrange").value_or(0);
 		return max & 0x0F; // range is in low 8 bits
 	}
 	return this->MaxValueAbs();
@@ -1424,10 +1424,12 @@ bool BR_Envelope::Commit (bool force /*=false*/)
 	if ((force || (m_update && !this->IsLocked())) && m_envelope)
 	{
 		// Prevents reselection of points in time selection
-		int envClickSegMode; GetConfig("envclicksegmode", envClickSegMode);
-		SetConfig("envclicksegmode", ClearBit(envClickSegMode, 6));
-		int pooledenvs; GetConfig("pooledenvs", pooledenvs);
-		SetConfig("pooledenvs", pooledenvs & (~12));
+		const ConfigVar<int> envClickSegMode("envclicksegmode");
+		ConfigVarOverride<int> tempEnvClickSegMode(envClickSegMode,
+			ClearBit(envClickSegMode.value_or(0), 6));
+
+		const ConfigVar<int> pooledenvs("pooledenvs");
+		ConfigVarOverride<int> tempPooledEnvs(pooledenvs, pooledenvs.value_or(0) & (~12));
 
 		const double playrate = m_take ? GetMediaItemTakeInfo_Value(m_take, "D_PLAYRATE") : 1;
 
@@ -1440,8 +1442,6 @@ bool BR_Envelope::Commit (bool force /*=false*/)
 		if (m_tempoMap)
 			UpdateTempoTimeline();
 
-		SetConfig("envclicksegmode", envClickSegMode);
-		SetConfig("pooledenvs", pooledenvs);
 		UpdateArrange();
 		m_update       = false;
 		m_pointsEdited = false;
@@ -2152,8 +2152,8 @@ WDL_FastString ConstructReceiveEnv (BR_EnvType type, double firstPointValue, boo
 	if (type != VOLUME && type != PAN && type != MUTE)
 		return envelope;
 
-	int defAutoMode; GetConfig("defautomode", defAutoMode);
-	int envLanes;    GetConfig("envlanes", envLanes);
+	const int defAutoMode = ConfigVar<int>("defautomode").value_or(0);
+	const int envLanes = ConfigVar<int>("envlanes").value_or(0);
 
 	BR_EnvShape defShape = (type == MUTE) ? SQUARE : GetDefaultPointShape();
 
@@ -2164,7 +2164,7 @@ WDL_FastString ConstructReceiveEnv (BR_EnvType type, double firstPointValue, boo
 	else if (type == PAN)    (hardwareSend) ? AppendLine(envelope, "<HWPANENV")  : AppendLine(envelope, "<AUXPANENV");
 	else if (type == MUTE)   (hardwareSend) ? AppendLine(envelope, "<HWMUTEENV") : AppendLine(envelope, "<AUXMUTEENV");
 
-	int volenvrange; GetConfig("volenvrange", volenvrange);
+	const int volenvrange = ConfigVar<int>("volenvrange").value_or(0);
 	envelope.AppendFormatted(128, "%s %d\n",             "ACT", 1);
 	envelope.AppendFormatted(128, "%s %d %d %d\n",       "VIS", 1, GetBit(envLanes, 0), 1);
 	envelope.AppendFormatted(128, "%s %d %d\n",          "LANEHEIGHT", 0, 0);
@@ -2306,7 +2306,7 @@ bool ToggleShowSendEnvelope (MediaTrack* track, int sendId, BR_EnvType type)
 						}
 
 						bool trim = false;
-						int trimMode; GetConfig("envtrimadjmode", trimMode);
+						const int trimMode = ConfigVar<int>("envtrimadjmode").value_or(0);
 						if (trimMode == 0 || (trimMode == 1 && GetEffectiveAutomationMode(hwSend ? track : CSurf_TrackFromID(sendTrackId + 1, false)) != 0))
 						{
 							trim = true;
@@ -2491,7 +2491,7 @@ bool ShowSendEnvelopes (vector<MediaTrack*>& tracks, BR_EnvType envelopeTypes)
 						}
 
 						bool trim = false;
-						int trimMode; GetConfig("envtrimadjmode", trimMode);
+						const int trimMode = ConfigVar<int>("envtrimadjmode").value_or(0);
 						if (trimMode == 0 || (trimMode == 1 && GetEffectiveAutomationMode(hwSend ? track : CSurf_TrackFromID(sendTrackId + 1, false)) != 0))
 						{
 							trim = true;
@@ -2628,7 +2628,7 @@ int CountTrackEnvelopePanels (MediaTrack* track)
 
 BR_EnvShape GetDefaultPointShape ()
 {
-	int defEnvs; GetConfig("defenvs", defEnvs);
+	const int defEnvs = ConfigVar<int>("defenvs").value_or(0);
 	return static_cast<BR_EnvShape>(defEnvs >> 16);
 }
 

Breeder/BR_MidiEditor.cpp
@@ -1031,7 +1031,7 @@ void ME_EnvPointsToCC (COMMAND_T* ct, int val, int valhw, int relmode, HWND hwnd
 				bool beatsTimebase = (midiEditor.GetTimebase() == PROJECT_BEATS || midiEditor.GetTimebase() == SOURCE_BEATS);
 
 				double stepSize = 0;
-				int midiCcDensity; GetConfig("midiccdensity", midiCcDensity);
+				const int midiCcDensity = ConfigVar<int>("midiccdensity").value_or(0);
 				if (midiCcDensity > 0)
 				{
 					stepSize = (double)midiEditor.GetPPQ() / (double)midiCcDensity;

Breeder/BR_Misc.cpp
@@ -70,7 +70,7 @@ static void MousePlaybackTimer ()
 {
 	// do not do this, it is generally unsafe: PreventUIRefresh(-1);
 	// (is this timer necessary with this removed?)
-	SetConfig("viewadvance", g_mousePlaybackReaperOptions);
+	ConfigVar<int>("viewadvance").try_set(g_mousePlaybackReaperOptions);
 	plugin_register("-timer", (void*)MousePlaybackTimer);
 }
 
@@ -84,7 +84,7 @@ static void MousePlaybackPlayState (bool play, bool pause, bool rec)
 	{
 		if (g_mousePlaybackRestoreView && g_activeCommand && (int)g_activeCommand->user > 0)
 		{
-			GetConfig("viewadvance", g_mousePlaybackReaperOptions);
+			g_mousePlaybackReaperOptions = ConfigVar<int>("viewadvance").value_or(0);
 			if (GetBit(g_mousePlaybackReaperOptions, 3))
 				g_mousePlaybackForceMoveView = true;
 		}
@@ -100,7 +100,7 @@ static void MousePlaybackPlayState (bool play, bool pause, bool rec)
 			//do not do this, it is generally unsafe: PreventUIRefresh(1);
 
 
-			SetConfig("viewadvance", ClearBit(g_mousePlaybackReaperOptions, 3));
+			ConfigVar<int>("viewadvance").try_set(g_mousePlaybackReaperOptions);
 			plugin_register("timer", (void*)MousePlaybackTimer);
 		}
 	}
@@ -290,8 +290,7 @@ static bool MousePlaybackInit (COMMAND_T* ct, bool init)
 
 		if (g_mousePlaybackRestoreView)
 		{
-			int viewAdvance;
-			GetConfig("viewadvance", viewAdvance);
+			const int viewAdvance = ConfigVar<int>("viewadvance").value_or(0);
 			if ((GetBit(viewAdvance, 3) || g_mousePlaybackForceMoveView) && (int)ct->user > 0) // Move view to edit cursor on stop (analogously applied here when starting playback from mouse cursor)
 			{
 				double arrangeStart, arrangeEnd;
@@ -1091,30 +1090,29 @@ void RestoreTrackSoloMuteStateSlot (COMMAND_T* ct)
 ******************************************************************************/
 void SnapFollowsGridVis (COMMAND_T* ct)
 {
-	int option;
-	GetConfig("projshowgrid", option);
-	SetConfig("projshowgrid", ToggleBit(option, 15));
+	if(ConfigVar<int> projshowgrid = "projshowgrid")
+		*projshowgrid = ToggleBit(*projshowgrid, 15);
 	RefreshToolbar(0);
 }
 
 void PlaybackFollowsTempoChange (COMMAND_T* ct)
 {
 	const char* configStr = "seekmodes";
-	int option; GetConfig(configStr, option);
+	ConfigVar<int> option(configStr);
+	if(!option) return;
 
-	option = ToggleBit(option, 5);
-	SetConfig(configStr, option);
+	*option = ToggleBit(*option, 5);
 	RefreshToolbar(0);
 
 	char tmp[256];
-	_snprintfSafe(tmp, sizeof(tmp), "%d", option);
+	_snprintfSafe(tmp, sizeof(tmp), "%d", *option);
 	WritePrivateProfileString("reaper", configStr, tmp, get_ini_file());
 }
 
 void TrimNewVolPanEnvs (COMMAND_T* ct)
 {
 	const char* configStr = "envtrimadjmode";
-	SetConfig(configStr, (int)ct->user);
+	ConfigVar<int>(configStr).try_set((int)ct->user);
 	RefreshToolbar(0);
 
 	char tmp[256];
@@ -1126,12 +1124,12 @@ void ToggleDisplayItemLabels (COMMAND_T* ct)
 {
 	const char* configStr = "labelitems2";
 
-	int option; GetConfig(configStr, option);
-	option = ToggleBit(option, (int)ct->user);
-	SetConfig(configStr, option);
+	ConfigVar<int> option(configStr);
+	if(!option) return;
+	*option = ToggleBit(*option, (int)ct->user);
 
 	char tmp[256];
-	_snprintfSafe(tmp, sizeof(tmp), "%d", option);
+	_snprintfSafe(tmp, sizeof(tmp), "%d", *option);
 	WritePrivateProfileString("reaper", configStr, tmp, get_ini_file());
 
 	UpdateArrange();
@@ -1140,13 +1138,13 @@ void ToggleDisplayItemLabels (COMMAND_T* ct)
 void SetMidiResetOnPlayStop (COMMAND_T* ct)
 {
 	const char* configStr = "midisendflags";
-	int option; GetConfig(configStr, option);
 
-	option = ToggleBit(option, (int)ct->user);
-	SetConfig(configStr, option);
+	ConfigVar<int> option(configStr);
+	if(!option) return;
+	*option = ToggleBit(*option, (int)ct->user);
 
 	char tmp[256];
-	_snprintfSafe(tmp, sizeof(tmp), "%d", option);
+	_snprintfSafe(tmp, sizeof(tmp), "%d", *option);
 	WritePrivateProfileString("reaper", configStr, tmp, get_ini_file());
 }
 
@@ -1155,41 +1153,39 @@ void SetOptionsFX (COMMAND_T* ct)
 	if ((int)ct->user == 1)
 	{
 		const char* configStr = "runallonstop";
-		int option; GetConfig(configStr, option);
+		ConfigVar<int> option(configStr);
 
 		// Set only if "Run FX when stopped" is turned on (otherwise the option is disabled so we don't allow the user to change it)
-		if (GetBit(option, 0))
+		if (option && GetBit(*option, 0))
 		{
-			option = ToggleBit(option, 3);
-			SetConfig(configStr, option);
+			*option = ToggleBit(*option, 3);
 
 			char tmp[256];
-			_snprintfSafe(tmp, sizeof(tmp), "%d", option);
+			_snprintfSafe(tmp, sizeof(tmp), "%d", *option);
 			WritePrivateProfileString("reaper", configStr, tmp, get_ini_file());
 		}
 	}
 	else if ((int)ct->user == 2)
 	{
 		const char* configStr = "loopstopfx";
-		int option; GetConfig(configStr, option);
+		ConfigVar<int> option(configStr);
+		if(!option) return;
 
-		option = ToggleBit(option, 0);
-		SetConfig(configStr, option);
+		*option = ToggleBit(*option, 0);
 
 		char tmp[256];
-		_snprintfSafe(tmp, sizeof(tmp), "%d", option);
+		_snprintfSafe(tmp, sizeof(tmp), "%d", *option);
 		WritePrivateProfileString("reaper", configStr, tmp, get_ini_file());
 	}
 	else
 	{
-		const char* configStr = "runallonstop";
-		int runallonstop; GetConfig(configStr, runallonstop);
+		const ConfigVar<int> runallonstop("runallonstop");
 
 		// Set only if "Run FX when stopped" is turned of (otherwise the option is disabled so we don't allow the user to change it)
-		if (!GetBit(runallonstop, 0))
+		if (runallonstop && !GetBit(*runallonstop, 0))
 		{
 			const char* configStr = "runafterstop";
-			SetConfig(configStr, abs((int)ct->user));
+			*ConfigVar<int>(configStr) = abs((int)ct->user);
 			char tmp[256];
 			_snprintfSafe(tmp, sizeof(tmp), "%d", abs((int)ct->user));
 			WritePrivateProfileString("reaper", configStr, tmp, get_ini_file());
@@ -1200,26 +1196,26 @@ void SetOptionsFX (COMMAND_T* ct)
 void SetMoveCursorOnPaste (COMMAND_T* ct)
 {
 	const char* configStr = "itemclickmovecurs";
-	int option; GetConfig(configStr, option);
 
-	option = ToggleBit(option, abs((int)ct->user));
-	SetConfig(configStr, option);
+	ConfigVar<int> option(configStr);
+	if(!option) return;
+	*option = ToggleBit(*option, abs((int)ct->user));
 
 	char tmp[256];
-	_snprintfSafe(tmp, sizeof(tmp), "%d", option);
+	_snprintfSafe(tmp, sizeof(tmp), "%d", *option);
 	WritePrivateProfileString("reaper", configStr, tmp, get_ini_file());
 }
 
 void SetPlaybackStopOptions (COMMAND_T* ct)
 {
-	const char* configStr = (((int)ct->user == 0) ? "stopprojlen" : "viewadvance");
-	int option; GetConfig(configStr, option);
+	const char* configStr = (int)ct->user == 0 ? "stopprojlen" : "viewadvance";
 
-	option = ToggleBit(option, ((int)ct->user));
-	SetConfig(configStr, option);
+	ConfigVar<int> option(configStr);
+	if(!option) return;
+	*option = ToggleBit(*option, (int)ct->user);
 
 	char tmp[256];
-	_snprintfSafe(tmp, sizeof(tmp), "%d", option);
+	_snprintfSafe(tmp, sizeof(tmp), "%d", *option);
 	WritePrivateProfileString("reaper", configStr, tmp, get_ini_file());
 }
 
@@ -1228,7 +1224,7 @@ void SetGridMarkerZOrder (COMMAND_T* ct)
 	const char* configStr = (((int)ct->user > 0) ? "gridinbg" : "gridinbg2");
 
 	int option = abs((int)ct->user) - 1;
-	SetConfig(configStr, option);
+	ConfigVar<int>(configStr).try_set(option);
 
 	char tmp[256];
 	_snprintfSafe(tmp, sizeof(tmp), "%d", option);
@@ -1246,13 +1242,13 @@ void SetAutoStretchMarkers(COMMAND_T* ct)
 
 void CycleRecordModes (COMMAND_T* ct)
 {
-	const char* configStr = "projrecmode";
-	int mode; GetConfig(configStr, mode);
-	if (++mode > 2) mode = 0;
+	ConfigVar<int> mode("projrecmode");
+	if(!mode) return;
+	if (++*mode > 2) mode = 0;
 
-	if      (mode == 0) Main_OnCommandEx(40253, 0, NULL);
-	else if (mode == 1) Main_OnCommandEx(40252, 0, NULL);
-	else if (mode == 2) Main_OnCommandEx(40076, 0, NULL);
+	if      (*mode == 0) Main_OnCommandEx(40253, 0, NULL);
+	else if (*mode == 1) Main_OnCommandEx(40252, 0, NULL);
+	else if (*mode == 2) Main_OnCommandEx(40076, 0, NULL);
 }
 
 /******************************************************************************
@@ -1562,31 +1558,31 @@ void ClearProjectTrackSelAction (COMMAND_T* ct)
 ******************************************************************************/
 int IsSnapFollowsGridVisOn (COMMAND_T* ct)
 {
-	int option; GetConfig("projshowgrid", option);
+	const int option = ConfigVar<int>("projshowgrid").value_or(0);
 	return !GetBit(option, 15);
 }
 
 int IsPlaybackFollowsTempoChangeOn (COMMAND_T* ct)
 {
-	int option; GetConfig("seekmodes", option);
+	const int option = ConfigVar<int>("seekmodes").value_or(0);
 	return GetBit(option, 5);
 }
 
 int IsTrimNewVolPanEnvsOn (COMMAND_T* ct)
 {
-	int option; GetConfig("envtrimadjmode", option);
+	const int option = ConfigVar<int>("envtrimadjmode").value_or(0);
 	return (option == (int)ct->user);
 }
 
 int IsToggleDisplayItemLabelsOn (COMMAND_T* ct)
 {
-	int option; GetConfig("labelitems2", option);
+	const int option = ConfigVar<int>("labelitems2").value_or(0);
 	return ((int)ct->user == 4) ? !GetBit(option, (int)ct->user) : GetBit(option, (int)ct->user);
 }
 
 int IsSetMidiResetOnPlayStopOn (COMMAND_T* ct)
 {
-	int option; GetConfig("midisendflags", option);
+	const int option = ConfigVar<int>("midisendflags").value_or(0);
 	return !GetBit(option, (int)ct->user);
 }
 
@@ -1594,37 +1590,37 @@ int IsSetOptionsFXOn (COMMAND_T* ct)
 {
 	if ((int)ct->user == 1)
 	{
-		int option; GetConfig("runallonstop", option);
+		const int option = ConfigVar<int>("runallonstop").value_or(0);
 		return GetBit(option, 0) ? GetBit(option, 3) : 0; // report as false if "Run FX when stopped" is turned off (because the option is then disabled in the preferences)
 	}
 	else if ((int)ct->user == 2)
 	{
-		int option; GetConfig("loopstopfx", option);
+		const int option = ConfigVar<int>("loopstopfx").value_or(0);
 		return GetBit(option, 0);
 	}
 	else
 	{
-		int runallonstop; GetConfig("runallonstop", runallonstop);
-		int option; GetConfig("runafterstop", option);
+		const int runallonstop = ConfigVar<int>("runallonstop").value_or(0);
+		const int option = ConfigVar<int>("runafterstop").value_or(0);
 		return GetBit(runallonstop, 0) ? 0 : option == abs((int)ct->user) ; // report as false if "Run FX when stopped" is turned on (because the option is then disabled in the preferences)
 	}
 }
 
 int IsSetMoveCursorOnPasteOn (COMMAND_T* ct)
 {
-	int option; GetConfig("itemclickmovecurs", option);
+	const int option = ConfigVar<int>("itemclickmovecurs").value_or(0);
 	return ((int)ct->user < 0) ? !GetBit(option, abs((int)ct->user)) : GetBit(option, abs((int)ct->user));
 }
 
 int IsSetPlaybackStopOptionsOn (COMMAND_T* ct)
 {
-	int option; GetConfig((((int)ct->user == 0) ? "stopprojlen" : "viewadvance"), option);
+	const int option = ConfigVar<int>((int)ct->user == 0 ? "stopprojlen" : "viewadvance").value_or(0);
 	return !!GetBit(option, (int)ct->user);
 }
 
 int IsSetGridMarkerZOrderOn (COMMAND_T* ct)
 {
-	int option; GetConfig((((int)ct->user > 0) ? "gridinbg" : "gridinbg2"), option);
+	const int option = ConfigVar<int>((int)ct->user > 0 ? "gridinbg" : "gridinbg2").value_or(0);
 
 	return option == abs((int)ct->user) - 1;
 }

commit 9db637bdb4a484ee9eca51e86cc2ccd0e0dbb308

fix _SWS_MOVECURSAMPLEFT and _SWS_MOVECURSAMPRIGHT [p=2282414]
Regression from v2.11.0: it broke in c06f8cfa0d64607c4544a733ae780af21a9d2439
by doing integer division instead of floating point division.

Misc/EditCursor.cpp
@@ -99,14 +99,15 @@ void MoveCursorSample(COMMAND_T* ct)
 	if (!projsrate)
 		return;
 
-	INT64 iCurSample = (INT64)(dPos * *projsrate + 0.5);
-	if (ct->user == -1 && (dPos == (double)(iCurSample / *projsrate)))
+	const double dSrate = *projsrate;
+	int64_t iCurSample = static_cast<int64_t>(dPos * dSrate + 0.5);
+
+	if (ct->user == -1 && (dPos == iCurSample / dSrate))
 		iCurSample--;
 	else if (ct->user == 1)
 		iCurSample++;
 
-	double dNewPos = (double)(iCurSample / *projsrate);
-
+	const double dNewPos = iCurSample / dSrate;
 	SetEditCurPos(dNewPos, true, false);
 }
 
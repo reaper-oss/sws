reaper-oss/sws

commit 30bd5b8a8edef9d64c2841913e812ee015f08afd

skip duplicate pooled MIDI sources when transposing or adjusting velocity
SWS/FNG: Increase/Decrease selected MIDI items velocity by x
SWS/FNG: Transpose selected MIDI items...

Fixes #1406

Fingers/RprMidiTake.cpp
@@ -1060,3 +1060,9 @@ bool RprMidiTake::hasEventType(RprMidiEvent::MessageType messageType)
     }
     return hasEvent(mOtherEvents, messageType);
 }
+
+std::string RprMidiTake::poolGuid() const
+{
+    const std::string &pooledevts = getMidiSourceNode()->getChild(1)->getValue();
+    return StringVector{pooledevts}.at(1);
+}

Fingers/MediaItemCommands.cpp
@@ -278,6 +278,7 @@ static bool convertToInProjectMidi(RprItemCtrPtr &ctr)
 
 void CmdPitchUpMidi::doCommand(int flag)
 {
+    std::set<std::string> pooledGuids;
     RprItemCtrPtr ctr = RprItemCollec::getSelected();
 
     if(!convertToInProjectMidi(ctr))
@@ -287,6 +288,8 @@ void CmdPitchUpMidi::doCommand(int flag)
         if (!ctr->getAt(i).getActiveTake().isMIDI())
             continue;
         RprMidiTake midiItem(ctr->getAt(i).getActiveTake());
+        if(!pooledGuids.insert(midiItem.poolGuid()).second)
+            continue;
         for(int j = 0; j < midiItem.countNotes(); j++) {
             RprMidiNote *note = midiItem.getNoteAt(j);
             note->setPitch(note->getPitch() + m_pitchAmt);
@@ -357,6 +360,7 @@ void CmdIncreaseItemRate::doCommand(int flag)
 
 void CmdVelChangeMidi::doCommand(int flag)
 {
+    std::set<std::string> pooledGuids;
     RprItemCtrPtr ctr = RprItemCollec::getSelected();
 
     if(!convertToInProjectMidi(ctr))
@@ -366,6 +370,8 @@ void CmdVelChangeMidi::doCommand(int flag)
         if (!ctr->getAt(i).getActiveTake().isMIDI())
             continue;
         RprMidiTake midiItem(ctr->getAt(i).getActiveTake());
+        if(!pooledGuids.insert(midiItem.poolGuid()).second)
+            continue;
         for(int j = 0; j < midiItem.countNotes(); j++) {
             RprMidiNote *note = midiItem.getNoteAt(j);
             note->setVelocity(note->getVelocity() + m_velAmt);

Fingers/RprMidiTake.h
@@ -110,6 +110,8 @@ public:
 
     bool hasEventType(RprMidiEvent::MessageType);
 
+    std::string poolGuid() const;
+
 private:
     void cleanup();
     bool apply();

Fingers/RprMidiTemplate.h
@@ -17,7 +17,7 @@ public:
 
     virtual ~RprMidiTemplate();
 protected:
-    RprNode *getMidiSourceNode() { return mMidiSourceNode; }
+    RprNode *getMidiSourceNode() const { return mMidiSourceNode; }
     void errorOccurred() { mInErrorState = true; }
     bool isReadOnly() const { return mReadOnly; }
 

commit 2fb2b9e7258d617d9d32b20ece13e6ac25c8b0fa

fix FNG MIDI actions skipping too much in REAPER v6 [p=2354538]
SWS/FNG: Increase/Decrease selected MIDI items velocity by x
SWS/FNG: Transpose selected MIDI items...

Regression from 30bd5b8a8edef9d64c2841913e812ee015f08afd.

Fingers/RprMidiTake.cpp
@@ -463,9 +463,8 @@ RprMidiCC::~RprMidiCC()
 
 static int getQNValue(RprNode *midiNode)
 {
-    const std::string &qnString = midiNode->getChild(0)->getValue();
-    StringVector tokens(qnString);
-    return ::atoi(tokens.at(2));
+    const std::string &hasdata = midiNode->findChildByToken("HASDATA")->getValue();
+    return ::atoi(StringVector{hasdata}.at(2));
 }
 
 static bool sortMidiBase(const RprMidiEvent *lhs, const RprMidiEvent *rhs)
@@ -1085,6 +1084,6 @@ bool RprMidiTake::hasEventType(RprMidiEvent::MessageType messageType)
 
 std::string RprMidiTake::poolGuid() const
 {
-    const std::string &pooledevts = getMidiSourceNode()->getChild(1)->getValue();
+    const std::string &pooledevts = getMidiSourceNode()->findChildByToken("POOLEDEVTS")->getValue();
     return StringVector{pooledevts}.at(1);
 }

Fingers/RprNode.cpp
@@ -49,6 +49,17 @@ RprNode *RprParentNode::getChild(int index) const
     return mChildren.at(index);
 }
 
+RprNode *RprParentNode::findChildByToken(const std::string &token) const
+{
+    for(RprNode *child : mChildren) {
+        const std::string &value = child->getValue();
+        if(value.rfind(token, 0) == 0 && value[token.size()] == '\x20')
+            return child;
+    }
+
+    return nullptr;
+}
+
 void RprParentNode::addChild(RprNode *node)
 {
     node->setParent(this);

Fingers/RprNode.h
@@ -13,6 +13,7 @@ public:
 
     virtual int childCount() const = 0;
     virtual RprNode *getChild(int index) const = 0;
+    virtual RprNode *findChildByToken(const std::string &) const = 0;
     virtual void addChild(RprNode *node) = 0;
     virtual void addChild(RprNode *node, int index) {}
     virtual void removeChild(int index) = 0;
@@ -32,6 +33,7 @@ public:
 
     int childCount() const override { return 0; }
     RprNode *getChild(int index) const override { return nullptr; }
+    RprNode *findChildByToken(const std::string &) const override { return nullptr; }
     void addChild(RprNode *node) override {}
     void removeChild(int index) override {}
 
@@ -49,6 +51,7 @@ public:
 
     int childCount() const override;
     RprNode *getChild(int index) const override;
+    RprNode *findChildByToken(const std::string &) const override;
     void addChild(RprNode *node) override;
     void addChild(RprNode *node, int index) override;
     void removeChild(int index) override;
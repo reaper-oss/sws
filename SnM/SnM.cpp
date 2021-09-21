777316a7dd1a485c5244788fd5fd0bffdd4e7b14
cfillion <cfillion@users.noreply.github.com>
2021-09-21 14:04:20 -0400

    don't increase the default action count

--- SnM/SnM.cpp
+++ SnM/SnM.cpp
@@ -765,10 +765,10 @@ static DYN_COMMAND_T s_dynCmdTable[]
 	{ "SWS: Select only track %d", "SWS_SEL", SelectTrack, 32, SNM_MAX_DYN_ACTIONS, },
 
 	// save/restore time & loop selection
-	{ "SWS: Save time selection, slot %d", "SWS_SAVETIME", SaveTimeSel, 16, SNM_MAX_DYN_ACTIONS},
-	{ "SWS: Restore time selection, slot %d", "SWS_RESTTIME", RestoreTimeSel, 16, SNM_MAX_DYN_ACTIONS},
-	{ "SWS: Save loop selection, slot %d", "SWS_SAVELOOP", SaveLoopSel, 16, SNM_MAX_DYN_ACTIONS},
-	{ "SWS: Restore loop selection, slot %d", "SWS_RESTLOOP", RestoreLoopSel, 16, SNM_MAX_DYN_ACTIONS},
+	{ "SWS: Save time selection, slot %d", "SWS_SAVETIME", SaveTimeSel, 5, SNM_MAX_DYN_ACTIONS},
+	{ "SWS: Restore time selection, slot %d", "SWS_RESTTIME", RestoreTimeSel, 5, SNM_MAX_DYN_ACTIONS},
+	{ "SWS: Save loop selection, slot %d", "SWS_SAVELOOP", SaveLoopSel, 5, SNM_MAX_DYN_ACTIONS},
+	{ "SWS: Restore loop selection, slot %d", "SWS_RESTLOOP", RestoreLoopSel, 5, SNM_MAX_DYN_ACTIONS},
 
 //!WANT_LOCALIZE_1ST_STRING_END
 

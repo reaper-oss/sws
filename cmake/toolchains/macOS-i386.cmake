93fc8440a930f80c14527b30df091c5f02c7886d
Mike Kot <to@myrrc.dev>
2023-02-22 22:32:57 +0300

    fix

--- cmake/toolchains/macOS-i386.cmake
+++ cmake/toolchains/macOS-i386.cmake
@@ -3,6 +3,6 @@ set(CMAKE_OSX_ARCHITECTURES i386)
 set(CMAKE_C_FLAGS -m32)
 set(CMAKE_CXX_FLAGS -stdlib=libc++ -m32)
 set(CMAKE_OSX_DEPLOYMENT_TARGET 10.7)
-#   ld: dynamic executables or dylibs must link with libSystem.dylib for architecture i386
+# ld: dynamic executables or dylibs must link with libSystem.dylib for architecture i386
 # 32bit builds are deprecated in newer macOS releases
 set(LINK_FLAGS "-L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib -lSystem")

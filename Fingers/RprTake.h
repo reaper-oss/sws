08d28f53f8b2652c82b1de0de18df7de0bbdee5c
Christian Fillion <cfillion@users.noreply.github.com>
2024-08-01 16:48:21 -0400

    fix a crash in various FNG MIDI actions when the active MIDI editor is empty (#1870)
    MIDIEditor_GetTake is null after deleting the MIDI editor's opened item if the editor is configured to be per-project and "Contents > Options when using one MIDI editor per project > Close editor when the active media item is deleted in arrange view" is disabled.
    
    The following actions were affected:
    
    - SWS/FNG: Apply groove to selected MIDI notes in active MIDI editor
    - SWS/FNG: Cycle through CC lanes in active MIDI editor
    - SWS/FNG: Get groove from selected MIDI notes in active MIDI editor
    - SWS/FNG: Hide unused CC lanes in active MIDI editor
    - SWS/FNG: Select muted MIDI notes
    - SWS/FNG: Select notes nearest edit cursor in active MIDI editor
    - SWS/FNG: Show only top CC lane in active MIDI editor
    - SWS/FNG: Show only used CC lanes in active MIDI editor
    - SWS/FNG: Show only used CC lanes in active MIDI editor
    
    Fixes #1869

--- Fingers/RprTake.h
+++ Fingers/RprTake.h
@@ -8,6 +8,7 @@ class PCM_source;
 class RprTake {
 public:
     RprTake(MediaItem_Take *take);
+    operator bool() const { return !!mTake; }
 
     double getPlayRate() const;
     void setPlayRate(double playRate);
@@ -26,8 +27,6 @@ public:
     
     void setName(const char *name);
     const char *getName();
-
-    static RprTake createFromMidiEditor();
 
     /* Conversion */
     MediaItem_Take *toReaper() const;

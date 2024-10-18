reaper-oss/sws

commit 9916097cc06d99071b351fa6ab544643781e6072

BR Mouse context: don't detect envelope segment when mouse cursor is outside of automation items bounds and underlying envelope is bypassed
fixes #1727, fixes #1488

Breeder/BR_MouseUtil.cpp
@@ -731,6 +731,18 @@ void BR_MouseInfo::GetContext (const POINT& p)
 						{
 							BR_Envelope envelope(mouseInfo.envelope);
 							trackEnvHit = this->IsMouseOverEnvelopeLine(envelope, height-2*ENV_GAP, offset+ENV_GAP, mouseDisplayX, mouseY, mousePos, arrangeStart, arrangeZoom, &mouseInfo.envPointId);
+
+							// if env is hit, check if underlying envelope outside of automation items is bypassed, #1727
+							if (trackEnvHit)
+							{
+								int bypassUnderlEnvProjDefault = *ConfigVar<int>("pooledenvattach") & 4;
+								int AIoptions = envelope.GetAIoptions(); // -1 == use project default
+								if ((bypassUnderlEnvProjDefault && AIoptions == -1) || (AIoptions & 4))
+								{
+									if (!this->IsMouseOverAI(envelope, height - 2 * ENV_GAP, offset + ENV_GAP, mouseY, mousePos))
+										trackEnvHit = 0;
+								}
+							}
 						}
 
 						if      (trackEnvHit == 1) mouseInfo.details = "env_point";
@@ -778,9 +790,21 @@ void BR_MouseInfo::GetContext (const POINT& p)
 						{
 							if      (trackEnvHit == 1) mouseInfo.details = "env_point";
 							else if (trackEnvHit == 2) mouseInfo.details = "env_segment";
+
+							// if env is hit, check if underlying envelope outside of automation items is bypassed, #1488
+							int bypassUnderlEnvProjDefault = *ConfigVar<int>("pooledenvattach") & 4;
+							BR_Envelope envelope(mouseInfo.envelope);
+							int AIoptions = envelope.GetAIoptions(); // -1 == use project default
+							if ((bypassUnderlEnvProjDefault && AIoptions == -1) || (AIoptions & 4))
+							{
+								if (!this->IsMouseOverAI(envelope, height - 2 * ENV_GAP, offset + ENV_GAP, mouseY, mousePos))
+								{
+									mouseInfo.details = "empty";
+								}
+							}
 						}
 						// Item and things inside it
-						else if (mouseInfo.item)
+						if (!trackEnvHit && mouseInfo.item)
 						{
 							// Take envelope takes priority
 							if (takeEnvHit != 0)
@@ -1309,6 +1333,24 @@ int BR_MouseInfo::IsMouseOverEnvelopeLine (BR_Envelope& envelope, int drawableEn
 	}
 	WritePtr(pointUnderMouse, pointId);
 	return mouseHit;
+}
+
+bool BR_MouseInfo::IsMouseOverAI(BR_Envelope & envelope, int drawableEnvHeight, int yOffset, int mouseY, double mousePos)
+{
+	// Check if mouse is in drawable part of envelope lane where line resides
+	if (mouseY >= yOffset && mouseY < yOffset + drawableEnvHeight)
+	{
+		TrackEnvelope* trEnv = envelope.GetPointer();
+		int AIcount = CountAutomationItems(trEnv);
+		for (int i = 0; i < AIcount; i++) {
+			double AIpos = GetSetAutomationItemInfo(trEnv, i, "D_POSITION", 0, false);
+			double AIlength = GetSetAutomationItemInfo(trEnv, i, "D_LENGTH", 0, false);
+
+			if (mousePos >= AIpos && mousePos < AIpos + AIlength)
+				return true;
+		}
+	}
+	return false;
 }
 
 int BR_MouseInfo::IsMouseOverEnvelopeLineTrackLane (MediaTrack* track, int trackHeight, int trackOffset, list<TrackEnvelope*>& laneEnvs, int mouseDisplayX, int mouseY, double mousePos, double arrangeStart, double arrangeZoom, TrackEnvelope** trackEnvelope, int* pointUnderMouse)

Breeder/BR_EnvelopeUtil.h
@@ -189,6 +189,7 @@ private:
 		EnvProperties& operator=  (const EnvProperties& properties);
 		vector<WDL_FastString> automationItems;
 		//POOLEDENVINST id pos length offset rate timeBased baseline(.5=0) amplitude loop ? ?
+		// see https://forum.cockos.com/showpost.php?p=2198410
 		// For now we're just storing as strings in properties and not handling parsing of these
 		//struct AutomationItem
 		//{

Breeder/BR_MouseUtil.h
@@ -159,6 +159,7 @@ private:
 	int IsMouseOverEnvelopeLine (BR_Envelope& envelope, int drawableEnvHeight, int yOffset, int mouseDisplayX, int mouseY, double mousePos, double arrangeStart, double arrangeZoom, int* pointUnderMouse);
 	int IsMouseOverEnvelopeLineTrackLane (MediaTrack* track, int trackHeight, int trackOffset, list<TrackEnvelope*>& laneEnvs, int mouseDisplayX, int mouseY, double mousePos, double arrangeStart, double arrangeZoom, TrackEnvelope** trackEnvelope, int* pointUnderMouse);
 	int IsMouseOverEnvelopeLineTake (MediaItem_Take* take, int takeHeight, int takeOffset, int mouseDisplayX, int mouseY, double mousePos, double arrangeStart, double arrangeZoom, TrackEnvelope** trackEnvelope, int* pointUnderMouse);
+	bool IsMouseOverAI(BR_Envelope& envelope, int drawableEnvHeight, int yOffset, int mouseY, double mousePos);
 	int GetRulerLaneHeight (int rulerH, int lane);
 	int IsHwndMidiEditor (HWND hwnd, HWND* midiEditor, HWND* subView);
 	static bool SortEnvHeightsById (const pair<int,int>& left, const pair<int,int>& right);

commit c74cfa10b7df481aea73cf83c2360244147abd01

repair BR_GetMouseCursorContext over envelopes lanes set to "Project default behavior outside of automation items"
Fixes #1908, regression from 9916097cc06d99071b351fa6ab544643781e6072

Breeder/BR_MouseUtil.cpp
@@ -624,13 +624,10 @@ void BR_MouseInfo::GetContext (const POINT& p)
 							// if env is hit, check if underlying envelope outside of automation items is bypassed, #1727
 							if (trackEnvHit)
 							{
-								int bypassUnderlEnvProjDefault = *ConfigVar<int>("pooledenvattach") & 4;
-								int AIoptions = envelope.GetAIoptions(); // -1 == use project default
-								if ((bypassUnderlEnvProjDefault && AIoptions == -1) || (AIoptions & 4))
-								{
-									if (!this->IsMouseOverAI(envelope, height - 2 * ENV_GAP, offset + ENV_GAP, mouseY, mousePos))
-										trackEnvHit = 0;
-								}
+								int aiOptions = envelope.GetAIoptions(); // -1 == use project default
+								if (aiOptions < 0) aiOptions = *ConfigVar<int>("pooledenvattach");
+								if (aiOptions & 4 && !this->IsMouseOverAI(envelope, height - 2 * ENV_GAP, offset + ENV_GAP, mouseY, mousePos))
+									trackEnvHit = 0;
 							}
 						}
 
@@ -681,16 +678,11 @@ void BR_MouseInfo::GetContext (const POINT& p)
 							else if (trackEnvHit == 2) mouseInfo.details = "env_segment";
 
 							// if env is hit, check if underlying envelope outside of automation items is bypassed, #1488
-							int bypassUnderlEnvProjDefault = *ConfigVar<int>("pooledenvattach") & 4;
 							BR_Envelope envelope(mouseInfo.envelope);
-							int AIoptions = envelope.GetAIoptions(); // -1 == use project default
-							if ((bypassUnderlEnvProjDefault && AIoptions == -1) || (AIoptions & 4))
-							{
-								if (!this->IsMouseOverAI(envelope, height - 2 * ENV_GAP, offset + ENV_GAP, mouseY, mousePos))
-								{
-									mouseInfo.details = "empty";
-								}
-							}
+							int aiOptions = envelope.GetAIoptions(); // -1 == use project default
+							if (aiOptions < 0) aiOptions = *ConfigVar<int>("pooledenvattach");
+							if (aiOptions & 4 && !this->IsMouseOverAI(envelope, height - 2 * ENV_GAP, offset + ENV_GAP, mouseY, mousePos))
+								mouseInfo.details = "empty";
 						}
 						// Item and things inside it
 						if (!trackEnvHit && mouseInfo.item)
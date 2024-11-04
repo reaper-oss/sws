reaper-oss/sws

commit 5b4b764f7d9a273ff51f2ce1d73cec2b95bd5069

store Auto Color and gradient colors in a platform-agnostic format
Changes the color storage format to 0x2RRGGBB everywhere. The extra bit indicates whether the new format is in use. (0x1000000 being already taken by REAPER and auto-color stores it in projects.)

Existing settings are converted automatically. Loading new settings with an old version of SWS will temporarily swap the colors on Windows until updated again.

Custom colors remain swapped between Windows and Linux because those are stored by REAPER.

Fixes #1811

Color/Color.cpp
@@ -43,9 +43,37 @@ COLORREF g_crGradStart = 0;
 COLORREF g_crGradEnd = 0;
 static bool g_bRecRedRuler = false;
 
+int SWS_ColorToNative(int xrgb)
+{
+#ifdef _WIN32
+	xrgb = (xrgb >> 16 & 0x000000ff) |
+	       (xrgb       & 0xff00ff00) |
+	       (xrgb << 16 & 0x00ff0000) ;
+#endif
+	return xrgb;
+}
+
+constexpr int PORTABLE_FLAG = 0x2000000;
+
+int ImportColor(int stored)
+{
+	if (stored <= 0)
+		return stored;
+	else if (!(stored & PORTABLE_FLAG))
+		stored = SWS_ColorFromNative(stored);
+
+	return stored & 0x1FFFFFF;
+}
+
+int ExportColor(const int rgb_or_neg)
+{
+	return rgb_or_neg > 0 ? rgb_or_neg | PORTABLE_FLAG : rgb_or_neg;
+}
+
 void UpdateCustomColors()
 {
 #ifndef __APPLE__
+	// no BGR/RGB conversion: setting owned by REAPER
 	GetPrivateProfileStruct("REAPER", "custcolors", g_custColors, sizeof(g_custColors), get_ini_file());
 #else
 	GetCustomColors(g_custColors);
@@ -64,9 +92,10 @@ bool AllBlack()
 void PersistColors()
 {
 	char str[256];
-	sprintf(str, "%d %d", g_crGradStart, g_crGradEnd);
+	sprintf(str, "%d %d", ExportColor(g_crGradStart), ExportColor(g_crGradEnd));
 	WritePrivateProfileString(SWS_INI, GRADIENT_COLOR_KEY, str, get_ini_file());
 #ifndef __APPLE__
+	// no BGR/RGB conversion: setting owned by REAPER
 	WritePrivateProfileStruct("REAPER", "custcolors", g_custColors, sizeof(g_custColors), get_ini_file());
 #else
 	SetCustomColors(g_custColors);
@@ -94,10 +123,10 @@ INT_PTR WINAPI doColorDlg(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			switch (pDI->CtlID)
 			{
 				case IDC_COLOR1:
-					hb = CreateSolidBrush(g_crGradStart);
+					hb = CreateSolidBrush(SWS_ColorToNative(g_crGradStart));
 					break;
 				case IDC_COLOR2:
-					hb = CreateSolidBrush(g_crGradEnd);
+					hb = CreateSolidBrush(SWS_ColorToNative(g_crGradEnd));
 					break;
 			}
 			FillRect(pDI->hDC, &pDI->rcItem, hb);
@@ -113,10 +142,10 @@ INT_PTR WINAPI doColorDlg(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 				switch (iSettingColor)
 				{
 				case 0:
-					g_crGradStart = cr;
+					g_crGradStart = SWS_ColorFromNative(cr);
 					break;
 				case 1:
-					g_crGradEnd = cr;
+					g_crGradEnd = SWS_ColorFromNative(cr);
 					break;
 				case 2:
 					UpdateCustomColors();
@@ -149,10 +178,10 @@ INT_PTR WINAPI doColorDlg(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 					if (wParam == IDC_COLOR1)
 					{
-						cc.rgbResult = g_crGradStart;
+						cc.rgbResult = SWS_ColorToNative(g_crGradStart);
 						if (ChooseColor(&cc))
 						{
-							g_crGradStart = cc.rgbResult;
+							g_crGradStart = SWS_ColorFromNative(cc.rgbResult) & 0xFFFFFF;
 							PersistColors();
 							RedrawWindow(hwndDlg, NULL, NULL, RDW_INVALIDATE | RDW_UPDATENOW);
 						}
@@ -171,13 +200,13 @@ INT_PTR WINAPI doColorDlg(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 						PersistColors();
 #elif !defined(__APPLE__)
 					COLORREF cc = 0;
-					if (wParam == IDC_COLOR1) cc = g_crGradStart;
-					if (wParam == IDC_COLOR2) cc = g_crGradEnd;
+					if (wParam == IDC_COLOR1) cc = SWS_ColorToNative(g_crGradStart);
+					if (wParam == IDC_COLOR2) cc = SWS_ColorToNative(g_crGradEnd);
 
 					if (SWELL_ChooseColor(hwndDlg,&cc,16,g_custColors))
 					{
-						if (wParam == IDC_COLOR1) g_crGradStart = cc;
-						if (wParam == IDC_COLOR2) g_crGradEnd = cc;
+						if (wParam == IDC_COLOR1) g_crGradStart = SWS_ColorFromNative(cc);
+						if (wParam == IDC_COLOR2) g_crGradEnd = SWS_ColorFromNative(cc);
 						PersistColors();
 						InvalidateRect(hwndDlg,NULL,FALSE);
 					}
@@ -214,12 +243,12 @@ INT_PTR WINAPI doColorDlg(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 							for (int i = 0; i < 16; i++)
 							{
 								sprintf(key, "custcolor%d", i+1);
-								sprintf(val, "%d", g_custColors[i]);
+								sprintf(val, "%d", ExportColor(SWS_ColorFromNative(g_custColors[i])));
 								WritePrivateProfileString("SWS Color", key, val, cFilename);
 							}
-							sprintf(val, "%d", g_crGradStart);
+							sprintf(val, "%d", ExportColor(g_crGradStart));
 							WritePrivateProfileString("SWS Color", "gradientStart", val, cFilename);
-							sprintf(val, "%d", g_crGradEnd);
+							sprintf(val, "%d", ExportColor(g_crGradEnd));
 							WritePrivateProfileString("SWS Color", "gradientEnd", val, cFilename);
 						}
 					}
@@ -253,7 +282,7 @@ INT_PTR WINAPI doColorDlg(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 								int iColor = GetPrivateProfileInt("SWS Color", key, -1, cPath);
 								if (iColor != -1)
 								{
-									g_custColors[i] = iColor;
+									g_custColors[i] = SWS_ColorToNative(ImportColor(iColor));
 									bFound = true;
 								}
 
@@ -267,6 +296,7 @@ INT_PTR WINAPI doColorDlg(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 							g_crGradStart = GetPrivateProfileInt("SWS Color", "gradientStart", g_crGradStart, cPath);
 							g_crGradEnd   = GetPrivateProfileInt("SWS Color", "gradientEnd", g_crGradEnd, cPath);
+							g_crGradStart = ImportColor(g_crGradStart), g_crGradEnd = ImportColor(g_crGradEnd);
 							PersistColors();
 #ifdef _WIN32
 							RedrawWindow(hwndDlg, NULL, NULL, RDW_INVALIDATE | RDW_UPDATENOW);
@@ -783,7 +813,7 @@ void TrackGradient(COMMAND_T* = NULL)
 		if (*(int*)GetSetMediaTrackInfo(tr, "I_SELECTED", NULL))
 		{
 			COLORREF cr = CalcGradient(g_crGradStart, g_crGradEnd, (double)iCurPos++ / (iNumSel-1)) | 0x1000000;
-			GetSetMediaTrackInfo(tr, "I_CUSTOMCOLOR", &cr);
+			SetMediaTrackInfo_Value(tr, "I_CUSTOMCOLOR", SWS_ColorToNative(cr));
 		}
 	}
 	Undo_OnStateChangeEx(__LOCALIZE("Set tracks to color gradient","sws_undo"), UNDO_STATE_TRACKCFG, -1);
@@ -824,7 +854,7 @@ void ItemTrackGrad(COMMAND_T* = NULL)
 				MediaItem* mi = GetTrackMediaItem(tr, j);
 				if (*(bool*)GetSetMediaItemInfo(mi, "B_UISEL", NULL))
 				{
-					COLORREF cr = g_crGradStart | 0x1000000;
+					COLORREF cr = SWS_ColorToNative(g_crGradStart | 0x1000000);
 					GetSetMediaItemInfo(mi, "I_CUSTOMCOLOR", &cr);
 				}
 			}
@@ -837,7 +867,7 @@ void ItemTrackGrad(COMMAND_T* = NULL)
 				if (*(bool*)GetSetMediaItemInfo(mi, "B_UISEL", NULL))
 				{
 					COLORREF cr = CalcGradient(g_crGradStart, g_crGradEnd, (double)iCurPos++ / (iNumSel-1)) | 0x1000000;
-					GetSetMediaItemInfo(mi, "I_CUSTOMCOLOR", &cr);
+					SetMediaItemInfo_Value(mi, "I_CUSTOMCOLOR", SWS_ColorToNative(cr));
 				}
 			}
 		}
@@ -868,7 +898,7 @@ void ItemGradient(COMMAND_T* = NULL)
 			if (*(bool*)GetSetMediaItemInfo(mi, "B_UISEL", NULL))
 			{
 				COLORREF cr = CalcGradient(g_crGradStart, g_crGradEnd, (double)iCurPos++ / (iNumSel-1)) | 0x1000000;
-				GetSetMediaItemInfo(mi, "I_CUSTOMCOLOR", &cr);
+				SetMediaItemInfo_Value(mi, "I_CUSTOMCOLOR", SWS_ColorToNative(cr));
 			}
 		}
 	}
@@ -890,7 +920,7 @@ void TakeGradient(COMMAND_T* = NULL)
 				for (int h = 0; h < CountTakes(mi); h++)
 				{
 					COLORREF cr = CalcGradient(g_crGradStart, g_crGradEnd, (double)tCurPos++ / (CountTakes(mi)-1)) | 0x1000000;
-					GetSetMediaItemTakeInfo(GetTake(mi, h), "I_CUSTOMCOLOR", &cr);
+					SetMediaItemTakeInfo_Value(GetTake(mi, h), "I_CUSTOMCOLOR", SWS_ColorToNative(cr));
 				}
 				tCurPos = 0;
 			}
@@ -1270,8 +1300,8 @@ int ColorInit()
 	LineParser lp(false);
 	if (!lp.parse(str))
 	{
-		g_crGradStart = lp.gettoken_int(0);
-		g_crGradEnd = lp.gettoken_int(1);
+		g_crGradStart = ImportColor(lp.gettoken_int(0));
+		g_crGradEnd = ImportColor(lp.gettoken_int(1));
 	}
 	g_bRecRedRuler = GetPrivateProfileInt(SWS_INI, RECREDRULER_KEY, g_bRecRedRuler, get_ini_file()) ? true : false;
 	if (g_bRecRedRuler) plugin_register("timer", (void*)ColorTimer);

Color/Autocolor.cpp
@@ -156,11 +156,7 @@ void SWS_AutoColorView::GetItemText(SWS_ListItem* item, int iCol, char* str, int
 		if (pItem->m_color<0 && (-pItem->m_color-1) < __ARRAY_SIZE(cColorTypes))
 			lstrcpyn(str, __localizeFunc(cColorTypes[-pItem->m_color-1],"sws_DLG_115",LOCALIZE_FLAG_NOCACHE), iStrMax);
 		else
-#ifdef _WIN32
-			snprintf(str, iStrMax, "0x%02x%02x%02x", pItem->m_color & 0xFF, (pItem->m_color >> 8) & 0xFF, (pItem->m_color >> 16) & 0xFF); //Brado: I think this is in reverse. Ini file dec to hex conversion (correct value for color) shows opposite of what this outputs to screen. e.g. 0xFFFF80 instead of 0x80FFFF
-#else
 			snprintf(str, iStrMax, "0x%06x", pItem->m_color);
-#endif
 		break;
 	case COL_ICON:
 		if (pItem->m_type == AC_TRACK)
@@ -197,11 +193,8 @@ void SWS_AutoColorView::SetItemText(SWS_ListItem* item, int iCol, const char* st
 					pItem->m_str_filter.Set(str);
 				break;
 			case COL_COLOR:
-			{
-				int iNewCol = strtol(str, NULL, 0);
-				pItem->m_color = RGB((iNewCol >> 16) & 0xFF, (iNewCol >> 8) & 0xFF, iNewCol & 0xFF);
-			}
-			break;
+				pItem->m_color = strtol(str, nullptr, 0) & 0xFFFFFF;
+				break;
 			case COL_TCP_LAYOUT:
 				pItem->m_layout[0].Set(str);
 				break;
@@ -389,12 +382,13 @@ void SWS_AutoColorWnd::OnCommand(WPARAM wParam, LPARAM lParam)
 				UpdateCustomColors();
 				cc.lpCustColors = g_custColors;
 				cc.Flags = CC_FULLOPEN | CC_RGBINIT;
-				cc.rgbResult = item->m_color;
+				cc.rgbResult = SWS_ColorToNative(item->m_color);
 				if (ChooseColor(&cc))
 				{
+					cc.rgbResult = SWS_ColorFromNative(cc.rgbResult & 0xFFFFFF); // ANDing for issue 600
 					int x = 0;
 					while ((item = (SWS_RuleItem*)m_pLists.Get(0)->EnumSelected(&x)))
-						item->m_color = cc.rgbResult & 0xFFFFFF; // fix for issue 600
+						item->m_color = cc.rgbResult;
 				}
 				Update();
 #elif defined(__APPLE__)
@@ -407,9 +401,10 @@ void SWS_AutoColorWnd::OnCommand(WPARAM wParam, LPARAM lParam)
 				COLORREF cc = item->m_color;
 				if (SWELL_ChooseColor(m_hwnd,&cc,16,g_custColors))
 				{
+					cc = SWS_ColorFromNative(cc & 0xFFFFFF);
 					int x = 0;
 					while ((item = (SWS_RuleItem*)m_pLists.Get(0)->EnumSelected(&x)))
-						item->m_color = cc & 0xFFFFFF;
+						item->m_color = cc;
 				}
 				Update();
 #endif
@@ -530,6 +525,7 @@ void SWS_AutoColorWnd::OnTimer(WPARAM wParam)
 	COLORREF cr;
 	if (m_bSettingColor && GetChosenColor(&cr))
 	{
+		cr &= 0xFFFFFF;
 		int x = 0;
 		SWS_RuleItem* item;
 		while ((item = (SWS_RuleItem*)m_pLists.Get(0)->EnumSelected(&x)))
@@ -566,7 +562,7 @@ INT_PTR SWS_AutoColorWnd::OnUnhandledMsg(UINT uMsg, WPARAM wParam, LPARAM lParam
 			while ((item = (SWS_RuleItem*)m_pLists.Get(0)->EnumSelected(&x)))
 			{
 				if (col < 0)
-					col = item->m_color;
+					col = SWS_ColorToNative(item->m_color);
 				else if (col != item->m_color)
 				{
 					col = -1;
@@ -721,7 +717,7 @@ void ApplyColorRuleToTrack(SWS_RuleItem* rule, bool bDoColors, bool bDoIcons, bo
 		PreventUIRefresh(1);
 
 		int iCount = 0;
-		WDL_PtrList<void> gradientTracks;
+		WDL_PtrList<MediaTrack> gradientTracks;
 
 		if (rule->m_color == -AC_CUSTOM-1)
 			UpdateCustomColors();
@@ -900,15 +896,15 @@ void ApplyColorRuleToTrack(SWS_RuleItem* rule, bool bDoColors, bool bDoIcons, bo
 							}
 						}
 						else
-							newCol = rule->m_color | 0x1000000;
+							newCol = SWS_ColorToNative(rule->m_color | 0x1000000);
 
 						// Only set the color if the user hasn't changed the color manually (but record it as being changed)
-						if ((bForce || iCurColor == pACTrack->m_col) && newCol != iCurColor)
+						if ((bForce || iCurColor == SWS_ColorToNative(pACTrack->m_col)) && newCol != iCurColor)
 						{
 							GetSetMediaTrackInfo(tr, "I_CUSTOMCOLOR", &newCol);
 						}
 
-						pACTrack->m_col = newCol;
+						pACTrack->m_col = SWS_ColorFromNative(newCol);
 						pACTrack->m_bColored = true;
 					}
 
@@ -977,12 +973,12 @@ void ApplyColorRuleToTrack(SWS_RuleItem* rule, bool bDoColors, bool bDoIcons, bo
 			if (i && gradientTracks.GetSize() > 1)
 				newCol = CalcGradient(g_crGradStart, g_crGradEnd, (double)i / (gradientTracks.GetSize()-1)) | 0x1000000;
 			for (int j = 0; j < g_pACTracks.Get()->GetSize(); j++)
-				if (g_pACTracks.Get()->Get(j)->m_pTr == (MediaTrack*)gradientTracks.Get(i))
+				if (g_pACTracks.Get()->Get(j)->m_pTr == gradientTracks.Get(i))
 				{
 					g_pACTracks.Get()->Get(j)->m_col = newCol;
 					break;
 				}
-			GetSetMediaTrackInfo((MediaTrack*)gradientTracks.Get(i), "I_CUSTOMCOLOR", &newCol);
+			SetMediaTrackInfo_Value(gradientTracks.Get(i), "I_CUSTOMCOLOR", SWS_ColorToNative(newCol));
 		}
 
 		PreventUIRefresh(-1);
@@ -1040,7 +1036,7 @@ void AutoColorTrack(bool bForce)
 				iCurColor = 0;
 
 			// Only remove color on tracks that we colored ourselves
-			if (pACTrack->m_col == iCurColor)
+			if (SWS_ColorToNative(pACTrack->m_col) == iCurColor)
 			{
 				GetSetMediaTrackInfo(pACTrack->m_pTr, "I_CUSTOMCOLOR", &g_i0);
 			}
@@ -1119,7 +1115,7 @@ void ApplyColorRuleToMarkerRegion(SWS_RuleItem* _rule, int _flags)
 				((_flags&AC_REGION && isRgn && _rule->m_type==AC_REGION) ||
 				(_flags&AC_MARKER && !isRgn && _rule->m_type==AC_MARKER)))
 			{
-				SetProjectMarkerByIndex(NULL, x-1, isRgn, pos, end, num, NULL, _rule->m_color==-AC_NONE-1 ? (isRgn?ct->marker:ct->region) : _rule->m_color | 0x1000000);
+				SetProjectMarkerByIndex(NULL, x-1, isRgn, pos, end, num, NULL, _rule->m_color==-AC_NONE-1 ? (isRgn?ct->marker:ct->region) : SWS_ColorToNative(_rule->m_color | 0x1000000));
 			}
 		}
 	}
@@ -1218,7 +1214,7 @@ static bool ProcessExtensionLine(const char *line, ProjectStateContext *ctx, boo
 				if (tr)
 				{
 					SWS_RuleTrack* rt = g_pACTracks.Get()->Add(new SWS_RuleTrack(tr));
-					rt->m_col = lp.gettoken_int(1);
+					rt->m_col = ImportColor(lp.gettoken_int(1));
 					rt->m_icon.Set(lp.gettoken_str(2));
 					rt->m_layout[0].Set(lp.gettoken_str(3));
 					rt->m_layout[1].Set(lp.gettoken_str(4));
@@ -1254,7 +1250,7 @@ static void SaveExtensionConfig(ProjectStateContext *ctx, bool isUndo, struct pr
 				if (CSurf_TrackToID(rt->m_pTr, false))
 					g = *(GUID*)GetSetMediaTrackInfo(rt->m_pTr, "GUID", NULL);
 				guidToString(&g, str);
-				ctx->AddLine("%s %d \"%s\" \"%s\" \"%s\"", str, rt->m_col, rt->m_icon.Get(), rt->m_layout[0].Get(), rt->m_layout[1].Get());
+				ctx->AddLine("%s %d \"%s\" \"%s\" \"%s\"", str, ExportColor(rt->m_col), rt->m_icon.Get(), rt->m_layout[0].Get(), rt->m_layout[1].Get());
 			}
 		}
 		ctx->AddLine(">");
@@ -1322,7 +1318,7 @@ int AutoColorInit()
 			WritePrivateProfileString(SWS_INI, key, NULL, get_ini_file());
 		LineParser lp(false);
 		if (!lp.parse(str) && lp.getnumtokens() >= 4)
-			g_pACItems.Add(new SWS_RuleItem(lp.gettoken_int(0), lp.gettoken_str(1), lp.gettoken_int(2), lp.gettoken_str(3), lp.gettoken_str(4), lp.gettoken_str(5)));
+			g_pACItems.Add(new SWS_RuleItem(lp.gettoken_int(0), lp.gettoken_str(1), ImportColor(lp.gettoken_int(2)), lp.gettoken_str(3), lp.gettoken_str(4), lp.gettoken_str(5)));
 		else if(!lp.parse(str) && lp.getnumtokens() == 3) //Reformat old format Autocolor line to new format (i.e. region + marker)
 			g_pACItems.Add(new SWS_RuleItem(AC_TRACK, lp.gettoken_str(0), lp.gettoken_int(1), lp.gettoken_str(2), "", ""));
 	}
@@ -1367,7 +1363,7 @@ void AutoColorSaveState()
 		makeEscapedConfigString(g_pACItems.Get(i)->m_icon.Get(), &tmp[1]);
 		makeEscapedConfigString(g_pACItems.Get(i)->m_layout[0].Get(), &tmp[2]);
 		makeEscapedConfigString(g_pACItems.Get(i)->m_layout[1].Get(), &tmp[3]);
-		snprintf(str, BUFFER_SIZE, "%d %s %d %s %s %s", g_pACItems.Get(i)->m_type, tmp[0].Get(), g_pACItems.Get(i)->m_color, tmp[1].Get(), tmp[2].Get(), tmp[3].Get());
+		snprintf(str, BUFFER_SIZE, "%d %s %d %s %s %s", g_pACItems.Get(i)->m_type, tmp[0].Get(), ExportColor(g_pACItems.Get(i)->m_color), tmp[1].Get(), tmp[2].Get(), tmp[3].Get());
 
 		snprintf(key, 32, AC_ITEM_KEY, i+1);
 		WritePrivateProfileString(SWS_INI, key, str, g_ACIni.Get());

Color/Color.h
@@ -26,9 +26,8 @@
 ******************************************************************************/
 #pragma once
 
-extern COLORREF g_custColors[16];
-extern COLORREF g_crGradStart;
-extern COLORREF g_crGradEnd;
+extern COLORREF g_custColors[16]; // in native format (RGB or BGR), shared with REAPER
+extern COLORREF g_crGradStart, g_crGradEnd; // in portable format (RGB)
 
 void UpdateCustomColors();
 void PersistColors();
@@ -37,3 +36,11 @@ COLORREF CalcGradient(COLORREF crStart, COLORREF crEnd, double dPos);
 int ColorInit();
 void ColorExit();
 void ShowColorDialog(COMMAND_T* = NULL);
+
+int SWS_ColorToNative(int xrgb);
+// int SWS_ColorFromNative(int bgr_or_rgb);
+#define SWS_ColorFromNative SWS_ColorToNative
+
+// convert to/from RGB with optional enable bit, or negative value
+int ImportColor(int stored);
+int ExportColor(int rgb_or_neg);

Misc/Adam.cpp
@@ -953,7 +953,7 @@ void AWDoAutoGroup(bool rec)
 				int rndColor;
 				if (g_AWAutoGroupRndColor)
 					// use WDL Mersenne Twister
-					rndColor = ColorToNative(g_MTRand.randInt(255), g_MTRand.randInt(255), g_MTRand.randInt(255)) | 0x1000000;
+					rndColor = RGB(g_MTRand.randInt(255), g_MTRand.randInt(255), g_MTRand.randInt(255)) | 0x1000000;
 
 				for (int i = 0; i < selItems.GetSize(); i++) {
 					MediaItem* item = selItems.Get()[i];
@@ -1026,7 +1026,7 @@ void NFDoAutoGroupTakesMode(WDL_TypedBuf<MediaItem*> origSelItems)
 
 	int rndColor;
 	if (g_AWAutoGroupRndColor)
-		rndColor = ColorToNative(g_MTRand.randInt(255), g_MTRand.randInt(255), g_MTRand.randInt(255)) | 0x1000000;
+		rndColor = RGB(g_MTRand.randInt(255), g_MTRand.randInt(255), g_MTRand.randInt(255)) | 0x1000000;
 
 	// do column-wise grouping
 	// loop through columns of items

sws_extension.cpp
@@ -742,7 +742,6 @@ error:
 /* unused
 		IMPAPI(Audio_RegHardwareHook);
 */
-		IMPAPI(ColorToNative)
 		IMPAPI(CoolSB_GetScrollInfo);
 		IMPAPI(CoolSB_SetScrollInfo);
 		IMPAPI(CountActionShortcuts);

commit b29d38e6aa55b98ff139c7a83a8670fac6caab83

repair setting the gradient end color on Windows (red/blue swapped since 2.14) [t=295534]
Regression from 5b4b764f7d9a273ff51f2ce1d73cec2b95bd5069

Color/Color.cpp
@@ -162,6 +162,15 @@ INT_PTR WINAPI doColorDlg(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		case WM_COMMAND:
 		{
 			wParam = LOWORD(wParam);
+
+			COLORREF *color;
+			switch(wParam)
+			{
+				case IDC_COLOR1: color = &g_crGradStart; break;
+				case IDC_COLOR2: color = &g_crGradEnd;   break;
+				default:         color = nullptr;        break;
+			}
+
 			switch(wParam)
 			{
 				case IDC_COLOR1:
@@ -176,22 +185,12 @@ INT_PTR WINAPI doColorDlg(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 					cc.lpCustColors = g_custColors;
 					cc.Flags = CC_FULLOPEN | CC_RGBINIT;
 
-					if (wParam == IDC_COLOR1)
+					if (color)
 					{
-						cc.rgbResult = SWS_ColorToNative(g_crGradStart);
+						cc.rgbResult = SWS_ColorToNative(*color);
 						if (ChooseColor(&cc))
 						{
-							g_crGradStart = SWS_ColorFromNative(cc.rgbResult) & 0xFFFFFF;
-							PersistColors();
-							RedrawWindow(hwndDlg, NULL, NULL, RDW_INVALIDATE | RDW_UPDATENOW);
-						}
-					}
-					else if (wParam == IDC_COLOR2)
-					{
-						cc.rgbResult = g_crGradEnd;
-						if (ChooseColor(&cc))
-						{
-							g_crGradEnd = cc.rgbResult;
+							*color = SWS_ColorFromNative(cc.rgbResult) & 0xFFFFFF;
 							PersistColors();
 							RedrawWindow(hwndDlg, NULL, NULL, RDW_INVALIDATE | RDW_UPDATENOW);
 						}
@@ -200,13 +199,11 @@ INT_PTR WINAPI doColorDlg(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 						PersistColors();
 #elif !defined(__APPLE__)
 					COLORREF cc = 0;
-					if (wParam == IDC_COLOR1) cc = SWS_ColorToNative(g_crGradStart);
-					if (wParam == IDC_COLOR2) cc = SWS_ColorToNative(g_crGradEnd);
+					if (color) cc = SWS_ColorToNative(*color);
 
 					if (SWELL_ChooseColor(hwndDlg,&cc,16,g_custColors))
 					{
-						if (wParam == IDC_COLOR1) g_crGradStart = SWS_ColorFromNative(cc);
-						if (wParam == IDC_COLOR2) g_crGradEnd = SWS_ColorFromNative(cc);
+						if (color) *color = SWS_ColorFromNative(cc);
 						PersistColors();
 						InvalidateRect(hwndDlg,NULL,FALSE);
 					}
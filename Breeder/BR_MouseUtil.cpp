reaper-oss/sws

commit bc45c9b959cc25d03f22afdc0ac0b1e37a35c0f0

remove unnecessary temporary vector from GetTrackOrEnvelopeFromY

Breeder/BR_MouseUtil.cpp
@@ -1476,45 +1476,30 @@ void BR_MouseInfo::GetTrackOrEnvelopeFromY (int y, TrackEnvelope** _envelope, Me
 	TrackEnvelope* envelope = NULL;
 	if (track)
 	{
+		const bool yInTrack = y < elementOffset + elementHeight;
+		const int count = CountTrackEnvelopes(track);
 		elementHeight = GetTrackHeightWithSpacer(track);
-		vector<pair<int,int>> envHeights;
-		bool yInTrack = (y < elementOffset + elementHeight) ? true : false;
 
-		int count = CountTrackEnvelopes(track);
 		for (int i = 0; i < count; ++i)
 		{
 			TrackEnvelope *env = GetTrackEnvelope(track, i);
+			const int envH = static_cast<int>(GetEnvelopeInfo_Value(env, "I_TCPH"));
+			if (envH < 1) continue;
 
-			if (GetEnvelopeInfo_Value(env,"I_TCPH") < 1.0) continue;
-			if (GetEnvelopeInfo_Value(env,"I_TCPY") < (elementHeight - spacer)) continue; // does not have an envcp
+			const int envY = static_cast<int>(GetEnvelopeInfo_Value(env, "I_TCPY"));
+			if (envY < elementHeight - spacer) continue; // does not have an envcp
 
 			if (yInTrack)
 			{
 				if (envelopes)
 					envelopes->push_back(env);
 			}
-			else
+			else if (y >= envY && y < envY + envH)
 			{
-				const int envHeight = static_cast<int>(GetEnvelopeInfo_Value(env,"I_TCPH"));
-				const int envId = i;
-				envHeights.push_back(make_pair(envHeight, envId));
-			}
-		}
-
-		if (!yInTrack)
-		{
-			int envelopeStart = elementOffset + elementHeight;
-			for (size_t i = 0; i < envHeights.size(); ++i)
-			{
-				int envelopeEnd = envelopeStart + envHeights[i].first;
-				if (y >= envelopeStart && y < envelopeEnd)
-				{
-					envelope = GetTrackEnvelope(track, envHeights[i].second);
-					elementHeight = envHeights[i].first;
-					elementOffset = envelopeStart;
-					break;
-				}
-				envelopeStart += envHeights[i].first;
+				envelope = env;
+				elementOffset = envY;
+				elementHeight = envH;
+				break;
 			}
 		}
 	}

commit e5e9d0b79f51a6a60fa69d482d6468ff5682255d

fix hit detection of envelopes in non-first tracks [p=2889179]
Regression from bc45c9b959cc25d03f22afdc0ac0b1e37a35c0f0

Breeder/BR_MouseUtil.cpp
@@ -1498,8 +1498,9 @@ void BR_MouseInfo::GetTrackOrEnvelopeFromY (int y, TrackEnvelope** _envelope, Me
 			const int envH = static_cast<int>(GetEnvelopeInfo_Value(env, "I_TCPH"));
 			if (envH < 1) continue;
 
-			const int envY = static_cast<int>(GetEnvelopeInfo_Value(env, "I_TCPY"));
+			int envY = static_cast<int>(GetEnvelopeInfo_Value(env, "I_TCPY"));
 			if (envY < elementHeight - spacer) continue; // does not have an envcp
+			envY += elementOffset;
 
 			if (yInTrack)
 			{
@@ -1510,7 +1511,7 @@ void BR_MouseInfo::GetTrackOrEnvelopeFromY (int y, TrackEnvelope** _envelope, Me
 			{
 				// report the height without the AI label gap, but still use the full height for detection under mouse
 				envelope = env;
-				elementOffset = static_cast<int>(GetEnvelopeInfo_Value(env, "I_TCPY_USED"));
+				elementOffset += static_cast<int>(GetEnvelopeInfo_Value(env, "I_TCPY_USED"));
 				elementHeight = static_cast<int>(GetEnvelopeInfo_Value(env, "I_TCPH_USED"));
 				break;
 			}